58440159e66772108242861510507e662809b38f
jeremyfelt <jeremy.felt@gmail.com>
2012-12-11 20:45:02 -0800

    Updated provisioning to include default nginx and php-fpm configs

--- precise/provision.sh
+++ precise/provision.sh
@@ -1,7 +1,15 @@
 apt-get update --force-yes -y
-apt-get upgrade --froce-yes -y
+apt-get upgrade --force-yes -y
 apt-get install --force-yes -y  php5 php5-fpm php-pear php5-common php5-mcrypt php5-mysql php5-cli php5-gd php-apc
+
+sudo cp /tmp/www.conf /etc/php5/fpm/pool.d/www.conf
+sudo /etc/init.d/php5-fpm restart
+
 apt-get install --force-yes -y nginx
+
+sudo cp /tmp/nginx.conf /etc/nginx/nginx.conf
+
+sudo /etc/init.d/nginx restart
 
 echo mysql-server mysql-server/root_password password blank | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password blank | sudo debconf-set-selections
