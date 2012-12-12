b4465c6d3b194e0823c56b5238dfcb0ff27512bb
jeremyfelt <jeremy.felt@gmail.com>
2012-12-11 19:33:10 -0800

    Initial add of first vagrant config

---
+++ precise/provision.sh
@@ -0,0 +1,15 @@
+apt-get update --force-yes -y
+apt-get upgrade --froce-yes -y
+apt-get install --force-yes -y  php5 php5-fpm php-pear php5-common php5-mcrypt php5-mysql php5-cli php5-gd php-apc
+apt-get install --force-yes -y nginx
+
+echo mysql-server mysql-server/root_password password blank | sudo debconf-set-selections
+echo mysql-server mysql-server/root_password_again password blank | sudo debconf-set-selections
+
+apt-get install --force-yes -y mysql-server php5-mysql
+apt-get install --force-yes -y git-core
+apt-get install --force-yes -y curl
+
+cp /srv/www/creditsesame/index.php /tmp/test.php
+
+echo All set!
