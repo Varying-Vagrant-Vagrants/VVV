431cbeb6e9736ea1309264f9e9ccbd23567fc887
Eric Mann <eric@eamann.com>
2013-01-08 16:26:49 -0800

    Fix typos in DB import script.

--- server-conf/db-dumps/import-sql.sh
+++ server-conf/db-dumps/import-sql.sh
@@ -4,6 +4,6 @@ cd /srv/server-conf/db-dumps/
 for file in $( ls *.sql )
 do
 pre_dot=${file%%.*}
-echo "mysql -u root -pblank $pre_dat < $pre_dot.sql"
-mysql -u root -pblank $pre_dot < $pre_dot.sql
+echo "mysql -u root -pblank $pre_dot < $pre_dot.sql"
+mysql -u root -pblank "$pre_dot" < $pre_dot.sql
 done
