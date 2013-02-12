09d3f5cda8cf759c3b69f2d01a3248e96fe60134
jeremyfelt <jeremy.felt@gmail.com>
2013-02-12 11:28:33 -0800

    Add piped comments after every file copy to avoid messy line endings
    This is cheap, but I think effective. In certain cases on Windows machines, it's possible that the line endings in the provision.sh get ugly. When the copy commands are run to move different c
    
    By adding a piped message to the end, we make sure that the filename is not what ends the statement. This should avoid any kind of weird filenames being copied into our config directories.

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,11 +1,11 @@
 # NGINX
 # Configure nginx with some basic config files
-sudo cp /srv/server-conf/nginx.conf /etc/nginx/nginx.conf
-sudo cp /srv/server-conf/nginx-wp-common.conf /etc/nginx/nginx-wp-common.conf
+sudo cp /srv/server-conf/nginx.conf /etc/nginx/nginx.conf | echo "copied nginx.conf to /etc/nginx/"
+sudo cp /srv/server-conf/nginx-wp-common.conf /etc/nginx/nginx-wp-common.conf | echo "copied nginx-wp-common.conf to /etc/nginx/"
 
 # Copy custom configuration files over and restart php5-fpm
-sudo cp /srv/server-conf/www.conf /etc/php5/fpm/pool.d/www.conf
-sudo cp /srv/server-conf/php.ini /etc/php5/fpm/php.ini
+sudo cp /srv/server-conf/www.conf /etc/php5/fpm/pool.d/www.conf | echo "copied www.conf to /etc/php5/fpm/pool.d/"
+sudo cp /srv/server-conf/php.ini /etc/php5/fpm/php.ini | echo "copied php.ini to /etc/php5/fpm/"
 
 # Make sure the services we expect to be running are running
 sudo service nginx restart
@@ -16,15 +16,11 @@ sudo service mysql restart
 # Import any SQL files into databases based on their names
 # these databases must first be created in the create-dbs.sql
 # file so that they exist for the import script to do its job.
-mysql -u root -pblank < /srv/server-conf/default-dbs.sql
-mysql -u root -pblank < /srv/server-conf/create-dbs.sql
+mysql -u root -pblank < /srv/server-conf/default-dbs.sql | echo "Imported default databases..."
+mysql -u root -pblank < /srv/server-conf/create-dbs.sql | echo "Created additional databases..."
 /srv/server-conf/db-dumps/import-sql.sh
 
-printf "\nCopy bash aliases to home directory"
-cp /srv/server-conf/bash_aliases /home/vagrant/.bash_aliases
-
-printf "\nUse start_config_monitor to begin monitoring config files for changes"
-printf "Use stop_config_monitor to stop monitoring these config files\n\n"
+cp /srv/server-conf/bash_aliases /home/vagrant/.bash_aliases | echo "Copied bash aliases to home directory..."
 
 # Your host IP is set in Vagrantfile, but it's nice to see the interfaces anyway.
 # Enter domains space delimited
