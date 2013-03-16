81c87622217bcc71ffd3daaff5f42671a37c4e7c
jeremyfelt <jeremy.felt@gmail.com>
2013-03-16 13:24:43 -0500

    Check for SQL files before for loop parsing those files
    Avoids an error when booting with a fresh `vagrant up` when no custom sql files have been added to /database/backups

--- database/import-sql.sh
+++ database/import-sql.sh
@@ -7,23 +7,28 @@ printf "\nStart DB Import\n\n"
 
 # Parse through each file in the directory and use the file name to
 # import the SQL file into the database of the same name
-for file in $( ls *.sql )
-do
-pre_dot=${file%%.*}
-mysql_cmd='SHOW TABLES FROM `'$pre_dot'`' # Required to support hypens in database names
-db_exist=`mysql -u root -pblank --skip-column-names -e "$mysql_cmd"`
-if [ "$?" != "0" ]
+if [ -f /srv/databases/backups/*.sql ]
 then
-	printf "Error - Create $pre_dot database via init-custom.sql before attempting import\n\n"
-else
-	if [ "" == "$db_exist" ]
+	for file in $( ls *.sql )
+	do
+	pre_dot=${file%%.*}
+	mysql_cmd='SHOW TABLES FROM `'$pre_dot'`' # Required to support hypens in database names
+	db_exist=`mysql -u root -pblank --skip-column-names -e "$mysql_cmd"`
+	if [ "$?" != "0" ]
 	then
-		printf "mysql -u root -pblank $pre_dot < $pre_dot.sql\n"
-		mysql -u root -pblank $pre_dot < $pre_dot.sql
-		printf "Import of $pre_dot successful\n\n"
+		printf "Error - Create $pre_dot database via init-custom.sql before attempting import\n\n"
 	else
-		printf "Skipped import of $pre_dot - tables exist\n\n"
+		if [ "" == "$db_exist" ]
+		then
+			printf "mysql -u root -pblank $pre_dot < $pre_dot.sql\n"
+			mysql -u root -pblank $pre_dot < $pre_dot.sql
+			printf "Import of $pre_dot successful\n\n"
+		else
+			printf "Skipped import of $pre_dot - tables exist\n\n"
+		fi
 	fi
+	done
+	printf "Databases imported\n"
+else
+	printf "No custom databases to import\n"
 fi
-done
-printf "Databases imported - press return for prompt\n"
