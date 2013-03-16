82022787bfc5dddc6f2b6016dfc067cbc1e396dd
jeremyfelt <jeremy.felt@gmail.com>
2013-03-16 00:22:40 -0500

    Clean up apt-get installs in provisioning script
    The provision script was so cleverly split up into many `apt-get install` statements, which was great but caused a little bit of a delay in processing each time so that it could gather the package data.
    
    This commit combines all of those packages into a bash array that we pass to one `apt-get install` command so that the entire shebang can be completed at once.
    
    In additon, some whitespace stuff has been cleaned up in the output because pretty output is better than ugly output.
    
    Closes #20

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,27 +1,19 @@
 # This provisioning script runs during a 'vagrant up' command when building
 # from a base box.
 
+# Check for our apt_update_run flag. If it exists, then we can skip apt-get update
+# and move on. If the flag has not yet been created, then we do want to update
+# first before touching the flag file and then installing packages.
 cat /srv/config/apt_update_run
 if [ "$?" == "0" ]
 then
-	printf "Skipping apt-get update, not initial boot...\n"
+	printf "\nSkipping apt-get update, not initial boot...\n\n"
 else
 	# update all of the package references before installing anything
-	printf "apt-get update....\n"
+	printf "apt-get update....\n\n"
 	apt-get update --force-yes -y
 	touch /srv/config/apt_update_run
 fi
-
-# Imagemagick
-apt-get install --force-yes -y imagemagick
-
-# PHP-FPM
-# kick of the install swarm with PHP5 and php-fpm
-apt-get install --force-yes -y  php5-fpm php-pear php5-common php5-imagick php5-mcrypt php5-mysql php5-curl php5-cli php5-gd php-apc php5-dev
-
-# NGINX
-# Install and configure nginx with some basic config files
-apt-get install --force-yes -y nginx
 
 # MYSQL
 # We need to set the selections to automatically fill the password prompt
@@ -30,21 +22,74 @@ apt-get install --force-yes -y nginx
 echo mysql-server mysql-server/root_password password blank | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password blank | sudo debconf-set-selections
 
-apt-get install --force-yes -y mysql-server
+# Build a bash array to pass all of the packages we want to install to
+# a single apt-get command. This avoids having to do all the leg work
+# each time a package is set to install. It also allows us to easily comment
+# out or add single packages.
+apt_package_list=(
+	# Imagemagick
+	imagemagick
 
-# MISC PACKAGES
-apt-get install --force-yes -y git-core curl make ngrep vim
+	# PHP5
+	#
+	# Our base packages for php5. As long as php5-fpm and php5-cli are
+	# installed, there is no need to install the general php5 package, which
+	# can sometimes install apache as a requirement.
+	php5-fpm
+	php5-cli
+	
+	# Common and dev packages for php
+	php5-common
+	php5-dev
 
+	# Extra modules that we find useful
+	php5-imagick
+	php5-mcrypt
+	php5-mysql
+	php5-curl
+	php-pear
+	php5-gd
+	php-apc
+	
+	# nginx
+	nginx
+
+	# mysql
+	mysql-server
+
+	# MISC Packages
+	git-core
+	curl
+	make
+	ngrep
+	vim
+
+	# memcached
+	memcached
+
+	# Install dos2unix, which allows conversion of DOS style line endings to
+	# something we'll have less trouble with in linux.
+	dos2unix
+)
+
+printf "Install all apt-get packages...\n"
+apt-get install --force-yes -y ${apt_package_list[@]}
+
+# Clean up apt caches
+apt-get clean
+
+printf "\nInstall pear packages...\n"
 # PHPUnit
 # We need turn on auto-discovery first, otherwise the system won't know where to grab PHPUnit from
 sudo pear config-set auto_discover 1
 sudo pear install pear.phpunit.de/PHPUnit
 
+printf "\nInstall pecl packages...\n"
 # MEMCACHED
 # Use memcached and the PECL memcache extension. At some point we can move
 # to the PECL memcached extension, but this better mirrors production
 # environments
-apt-get install --force-yes -y memcached
+#
 # We have to enter yes once. If this install changes, this will no longer work
 # Might consider installing yum package mamager and using yum to install to get
 # around this requiremnt. Without this, the script stalls upon completion.
@@ -54,13 +99,7 @@ printf "yes\n" | pecl install memcache
 # Install XDebug for PHP
 printf "yes\n" | pecl install xdebug
 
-# Install dos2unix, which allows conversion of DOS style line endings to
-# something we'll have less trouble with in linux.
-sudo apt-get install --force-yes -y dos2unix
-
-# Clean up apt caches
-sudo apt-get clean
-
+printf "\nLink Directories...\n"
 # NGINX
 # Configure nginx with some basic config files
 sudo ln -sf /srv/config/nginx-config/nginx.conf /etc/nginx/nginx.conf | echo "Linked nginx.conf to /etc/nginx/"
@@ -74,6 +113,7 @@ sudo ln -sf /srv/config/php5-fpm-config/php.xdebug.ini /etc/php5/fpm/php.xdebug.
 # Copy over the mysql configuration file
 sudo cp /srv/config/mysql-config/my.cnf /etc/mysql/my.cnf | echo "Linked my.cnf to /etc/mysql/"
 
+printf "\nRestart services...\n"
 # Make sure the services we expect to be running are running
 sudo service nginx restart
 sudo service php5-fpm restart
@@ -82,7 +122,7 @@ sudo service mysql restart
 
 # Create the databases (unique to system) that will be imported with
 # the mysqldump files located in database/backups/
-mysql -u root -pblank < /srv/database/init-custom.sql | echo "Initial custom mysql scripting..."
+mysql -u root -pblank < /srv/database/init-custom.sql | printf "\nInitial custom mysql scripting...\n"
 
 # Setup mysql by importing an init file that creates necessary
 # users and databases that our vagrant setup relies on.
