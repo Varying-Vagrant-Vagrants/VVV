e6ca261059b4b6ffbb1004b0636245437a6b5579
Daniel Bachhuber <d@danielbachhuber.com>
2013-05-14 16:40:18 -0700

    tools.pp: We need `git` and `svn`, of course.

--- manifests/tools.pp
+++ manifests/tools.pp
@@ -1,7 +1,26 @@
+# Version control systems
+
+package { 'git':
+	ensure		=> present,
+	name 		=> 'git',
+}
+
+package { 'subversion':
+	ensure		=> present,
+	name 		=> 'subversion',
+}
+
+
+
+# Editors
+
 package { 'vim':
 	ensure		=> present,
 	name 		=> 'vim',
 }
+
+
+# Misc CLI tools
 
 package { 'ack-grep':
 	ensure		=> present,
