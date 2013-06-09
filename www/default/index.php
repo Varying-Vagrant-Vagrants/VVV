7d2c066ea734bfbd2e6f52aaebd0c354aa4339e9
jeremyfelt <jeremy.felt@gmail.com>
2013-06-09 16:09:12 -0700

    Start getting ready for a Boostrappy display

--- www/default/index.php
+++ www/default/index.php
@@ -1,4 +1,14 @@
-<?php
-
-echo '<h1>Welcome to Varying Vagrant Vagrants!</h1>';
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Varying Vagrant Vagrants Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Welcome to Varying Vagrant Vagrants</h1>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
