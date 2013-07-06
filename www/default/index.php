fadc710c4f27f1cd03002663ce52fbcbd402a7fb
jeremyfelt <jeremy.felt@gmail.com>
2013-07-05 20:31:00 -0700

    Remove poor attempt at Bootstrapifying the dashboard.
    This was poor judgement. We should have a vision for the dashboard screen before we start assinging styles to it. I think this was me being lazy.
    
    I'm pulling this out in advance of the 0.8 release. We should have a dashboard discussion in an issue.

--- www/default/index.php
+++ www/default/index.php
@@ -3,40 +3,14 @@
 <head>
 	<title>Varying Vagrant Vagrants Dashboard</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Bootstrap -->
-	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-	<style>
-		body {
-			padding-top: 60px;
-		}
-	</style>
 </head>
 <body>
-	<div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container-fluid">
-				<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="brand" href="#">Varying Vagrant Vagrants</a>
-				<div class="nav-collapse collapse">
-					<ul class="nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="http://github.com/10up/varying-vagrant-vagrants">Repository</a></li>
-						<li><a href="database-admin/">phpMyAdmin</a></li>
-						<li><a href="memcached-admin/">phpMemcachedAdmin</a></li>
-						<li><a href="phpinfo/">PHP Info</a></li>
-					</ul>
-				</div><!--/.nav-collapse -->
-			</div>
-		</div>
-	</div>
-	<div class="container">
-		<h1>Welcome to Varying Vagrant Vagrants</h1>
-	</div>
-	<script src="http://code.jquery.com/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+<ul class="nav">
+	<li class="active"><a href="#">Home</a></li>
+	<li><a href="http://github.com/10up/varying-vagrant-vagrants">Repository</a></li>
+	<li><a href="database-admin/">phpMyAdmin</a></li>
+	<li><a href="memcached-admin/">phpMemcachedAdmin</a></li>
+	<li><a href="phpinfo/">PHP Info</a></li>
+</ul>
 </body>
 </html>
