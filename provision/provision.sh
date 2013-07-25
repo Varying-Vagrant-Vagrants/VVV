23082a9fdf2acb4104bb2f10ffaf2f71224de876
jeremyfelt <jeremy.felt@gmail.com>
2013-07-24 17:37:24 -0700

    Output package versions during provisioning when already installed
    Outputs package versions after checking to see if they are installed during provisioning. Attempts to format things nicely, though there's more than likely room for improvement there.

--- provision/provision.sh
+++ provision/provision.sh
@@ -90,11 +90,15 @@ echo "Check for packages to install..."
 # not yet installed, it should be added to the array of packages to install.
 for pkg in "${apt_package_check_list[@]}"
 do
-	if dpkg -s $pkg 2>&1 | grep -q 'Status: install ok installed';
+	package_version=`dpkg -s $pkg 2>&1 | grep 'Version:' | cut -d " " -f 2`
+	if [[ $package_version != "" ]]
 	then 
-		echo "  * " $pkg already installed
+		space_count=`expr 20 - "${#pkg}"` #11
+		pack_space_count=`expr 30 - "${#package_version}"`
+		real_space=`expr ${space_count} + ${pack_space_count} + ${#package_version}`
+		printf " * $pkg %${real_space}.${#package_version}s" $package_version
 	else
-		echo "  * " $pkg not yet installed
+		echo " *" $pkg [not installed]
 		apt_package_install_list+=($pkg)
 	fi
 done
