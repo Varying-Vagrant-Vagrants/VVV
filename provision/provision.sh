90cb177156976ff93a571acef10e23a93a4a1ba2
Aaron Jorbin <aaron@jorb.in>
2013-08-25 16:23:10 -0400

    check for instilation of grunt rather than if the directory exists

--- provision/provision.sh
+++ provision/provision.sh
@@ -224,13 +224,13 @@ then
 	#
 	# Install or Update Grunt based on gurrent state.  Updates are direct
 	# from NPM
-	if [ ! -d /usr/lib/node_modules/grunt-cli  ]
+	if grunt --version ;
 	then
-		echo "Installing Grunt CLI"
-		npm install -g grunt-cli
-	else
 		echo "Updating Grunt CLI"
 		npm update -g grunt-cli
+	else
+		echo "Installing Grunt CLI"
+		npm install -g grunt-cli
 	fi
 
 else
