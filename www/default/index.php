4defb16be36e04f8718ee57bd6e945e47832d61b
jeremyfelt <jeremy.felt@gmail.com>
2013-11-07 21:16:09 -0800

    Allow for a custom dashboard to replace the vvv.dev default
    Uses a file_exists check to load `dashboard-custom.php` if it's available so that a custom dashboard can be provided for VVV
    
    Closes #185

--- www/default/index.php
+++ www/default/index.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * If a custom dashboard file exists, load that instead of the default
+ * dashboard provided by Varying Vagrant Vagrants. This file should be
+ * located in the `www/default/` directory.
+ */
+if ( file_exists( 'dashboard-custom.php' ) ) {
+	include( 'dashboard-custom.php' );
+	exit;
+}
+
+// Begin default dashboard.
+?>
 <!DOCTYPE html>
 <html>
 <head>
