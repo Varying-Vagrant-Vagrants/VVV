ccb9ce49b9a9094bcde4551697866131d8e08b56
jeremyfelt <jeremy.felt@gmail.com>
2013-11-25 21:13:18 -0800

    Remove Ruby and SASS, as they are no longer required
    The switch to grunt-sass in the WordPress develop.svn removed any dependency on Ruby or the SASS gem.

--- provision/provision.sh
+++ provision/provision.sh
@@ -94,10 +94,6 @@ apt_package_check_list=(
 	# nodejs for use by grunt
 	g++
 	nodejs
-
-	# Ruby is needed for SASS
-	ruby
-
 )
 
 echo "Check for apt packages to install..."
@@ -256,17 +252,6 @@ if [[ $ping_result == *bytes?from* ]]; then
 		npm install -g grunt-sass &>/dev/null
 		npm install -g grunt-cssjanus &>/dev/null
 	fi
-
-	if sass -v;
-	then 
-		echo "updating sass"
-		gem update sass
-	else
-		echo "installing sass"
-		gem install sass --pre
-	fi
-
-
 else
 	echo -e "\nNo network connection available, skipping package installation"
 fi
