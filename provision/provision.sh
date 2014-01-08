93484d7cc9242400cd08058a3c97e711d5fc3e3b
John P. Bloch <john.bloch@10up.com>
2014-01-08 08:28:16 -0500

    Set the bin-dir config value after adding requirements

--- provision/provision.sh
+++ provision/provision.sh
@@ -205,6 +205,7 @@ if [[ $ping_result == *bytes?from* ]]; then
 		COMPOSER_HOME=/usr/local/src/composer composer global require --no-update phpunit/phpunit:3.7.*
 		COMPOSER_HOME=/usr/local/src/composer composer global require --no-update mockery/mockery:0.8.*
 		COMPOSER_HOME=/usr/local/src/composer composer global require --no-update d11wtq/boris:v1.0.2
+		COMPOSER_HOME=/usr/local/src/composer composer global config bin-dir /usr/local/bin
 		COMPOSER_HOME=/usr/local/src/composer composer global update
 	fi
 
