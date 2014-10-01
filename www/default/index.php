810334fb03ec545f6746175bace8eb970fff1b79
Joseph Richardson <jrichardson@zingermans.com>
2014-09-30 23:14:24 -0400

    Adds mailcatcher support.
    Close #337
    Close #464 past PR based on master
    
    Includes:
     RVM install
     mailcatcher install
     startup script
     PHP smtp configuration
     dashboard link to port
    
    Later revisions also did the following:
    Fixes issues with signature checks in the RVM installation process.
    Removed the stopgap fix for i18n dependency issues of past.
    Added blank lines per request.

--- www/default/index.php
+++ www/default/index.php
@@ -24,6 +24,7 @@ if ( file_exists( 'dashboard-custom.php' ) ) {
 	<li><a href="database-admin/">phpMyAdmin</a></li>
 	<li><a href="memcached-admin/">phpMemcachedAdmin</a></li>
 	<li><a href="opcache-status/opcache.php">Opcache Status</a></li>
+	<li><a href="http://vvv.dev:1080">Mailcatcher</a></li>
 	<li><a href="webgrind/">Webgrind</a></li>
 	<li><a href="phpinfo/">PHP Info</a></li>
 </ul>
