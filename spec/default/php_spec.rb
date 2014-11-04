36aa98a07613a2156225aa52637c32bcc7a7ea5f
miya0001 <miya@wpist.me>
2014-11-04 18:35:38 +0900

    add tests for php-custom.ini

--- spec/default/php_spec.rb
+++ spec/default/php_spec.rb
@@ -1,25 +1,22 @@
 require 'spec_helper'
-require 'shellwords'
 
-
-describe 'PHP config parameters' do
-  # context  php_config('default_charset') do
-  #   its(:value) { should eq 'UTF-8' }
-  # end
-  #
-  # context  php_config('mbstring.language') do
-  #   its(:value) { should eq 'neutral' }
-  # end
-  #
-  # context  php_config('mbstring.internal_encoding') do
-  #   its(:value) { should eq 'UTF-8' }
-  # end
-  #
-  # context php_config('date.timezone') do
-  #   its(:value) { should eq 'UTC' }
-  # end
-  #
-  # context php_config('short_open_tag') do
-  #   its(:value) { should eq 'Off' }
-  # end
+describe file('/etc/php5/fpm/conf.d/php-custom.ini') do
+  it { should be_file }
+  it { should contain(/short_open_tag = Off/) }
+  it { should contain(/allow_call_time_pass_reference = Off/) }
+  it { should contain(/max_execution_time = 30/) }
+  it { should contain(/memory_limit = 128M/) }
+  it { should contain(/error_reporting = E_ALL | E_STRICT/) }
+  it { should contain(/display_errors = On/) }
+  it { should contain(/log_errors = On/) }
+  it { should contain(/log_errors_max_len = 1024/) }
+  it { should contain(/ignore_repeated_errors = Off/) }
+  it { should contain(/ignore_repeated_source = Off/) }
+  it { should contain(/track_errors = Off/) }
+  it { should contain(/html_errors = 1/) }
+  it { should contain(/error_log = \/srv\/log\/php_errors.log/) }
+  it { should contain(/post_max_size = 50M/) }
+  it { should contain(/upload_max_filesize = 50M/) }
+  it { should contain(/max_file_uploads = 20/) }
+  it { should contain(/default_socket_timeout = 60/) }
 end
