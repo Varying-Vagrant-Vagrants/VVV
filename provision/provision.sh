ce7589b1fb4df8b976b63b07fc5dfff408562150
jeremyfelt <jeremy.felt@gmail.com>
2014-11-09 11:08:07 -0800

    global bin-dir is required for Composer packages

--- provision/provision.sh
+++ provision/provision.sh
@@ -204,6 +204,7 @@ if [[ $ping_result == *bytes?from* ]]; then
 		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update phpunit/php-invoker:1.1.*
 		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update mockery/mockery:0.9.*
 		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update d11wtq/boris:v1.0.8
+		COMPOSER_HOME=/usr/local/src/composer composer -q global config bin-dir /usr/local/bin
 		COMPOSER_HOME=/usr/local/src/composer composer global update
 	fi
 
