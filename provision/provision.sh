4dd19cfd6d0259e8e9e14c3b07b3b3e45dd48339
jeremyfelt <jeremy.felt@gmail.com>
2014-11-09 11:16:32 -0800

    Upgrade PHPUnit to 4.3.x

--- provision/provision.sh
+++ provision/provision.sh
@@ -200,7 +200,7 @@ if [[ $ping_result == *bytes?from* ]]; then
 	if [[ -n "$(composer --version --no-ansi | grep 'Composer version')" ]]; then
 		echo "Updating Composer..."
 		COMPOSER_HOME=/usr/local/src/composer composer self-update
-		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update phpunit/phpunit:4.2.*
+		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update phpunit/phpunit:4.3.*
 		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update phpunit/php-invoker:1.1.*
 		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update mockery/mockery:0.9.*
 		COMPOSER_HOME=/usr/local/src/composer composer -q global require --no-update d11wtq/boris:v1.0.8
