87484b3b7ce5314fea9dcfdc8f3959edb3cf270a
jeremyfelt <jeremy.felt@gmail.com>
2014-11-12 20:48:03 -0800

    Update docs around Graphviz symlink

--- provision/provision.sh
+++ provision/provision.sh
@@ -225,14 +225,14 @@ if [[ $ping_result == *bytes?from* ]]; then
 		npm install -g grunt-sass &>/dev/null
 		npm install -g grunt-cssjanus &>/dev/null
 	fi
-	
-	# graphviz
+
+	# Graphviz
 	#
-	# Set up a symlink between the graphviz path defined in the default Webgrind
-	# config and the apt-get install path
-	echo "Adding graphviz symlink for Webgrind"
+	# Set up a symlink between the Graphviz path defined in the default Webgrind
+	# config and actual path.
+	echo "Adding graphviz symlink for Webgrind..."
 	ln -sf /usr/bin/dot /usr/local/bin/dot
-	
+
 else
 	echo -e "\nNo network connection available, skipping package installation"
 fi
