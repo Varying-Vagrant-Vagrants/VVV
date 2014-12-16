0cea330b562120020a894e2083cc98f1ce0c2a44
Stephen Edgar <stephen@netweb.com.au>
2014-12-16 16:10:44 +1100

    Don't ignore SVN externals for WordPress /trunk
    If a new version of Akismet is shipped let `svn up` grab that latest version for WordPress `/trunk`

--- provision/provision.sh
+++ provision/provision.sh
@@ -518,7 +518,7 @@ PHP
 	else
 		echo "Updating WordPress trunk..."
 		cd /srv/www/wordpress-trunk
-		svn up --ignore-externals
+		svn up
 	fi
 
 	# Checkout, install and configure WordPress trunk via develop.svn
