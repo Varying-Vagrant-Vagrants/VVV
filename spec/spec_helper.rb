df28945b1ea04073fb8113aae027a5aae3bfb24b
miya0001 <miya@wpist.me>
2015-05-21 12:36:07 +0900

    update spec_helper to executing on the localhost

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,4 @@
 require 'serverspec'
-require 'net/ssh'
-require 'tempfile'
 
-set :backend, :ssh
+set :backend, :exec
 
-if ENV['ASK_SUDO_PASSWORD']
-  begin
-    require 'highline/import'
-  rescue LoadError
-    fail "highline is not available. Try installing it."
-  end
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
-else
-  set :sudo_password, ENV['SUDO_PASSWORD']
-end
-
-host = ENV['TARGET_HOST']
-
-`vagrant up #{host}`
-
-config = Tempfile.new('', Dir.tmpdir)
-`vagrant ssh-config #{host} > #{config.path}`
-
-options = Net::SSH::Config.for(host, [config.path])
-
-options[:user] ||= Etc.getlogin
-
-set :host,        options[:host_name] || host
-set :ssh_options, options
-
-# Disable sudo
-# set :disable_sudo, true
-
-
-# Set environment variables
-# set :env, :LANG => 'C', :LC_MESSAGES => 'C' 
-
-# Set PATH
-# set :path, '/sbin:/usr/local/sbin:$PATH'
