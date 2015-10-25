6ddd8c3be6871464789e58f7ad55dff4cde236c1
Jeremy Felt <jeremy.felt@gmail.com>
2015-10-25 12:38:15 -0700

    Update Webgrind repository to forked version supporting Xdebug 2.3
    The original Webgrind repository does not show full function names
    when viewing cachegrind output. This fork resolves the issue.

--- provision/provision.sh
+++ provision/provision.sh
@@ -545,8 +545,8 @@ webgrind_install() {
   # Webgrind install (for viewing callgrind/cachegrind files produced by
   # xdebug profiler)
   if [[ ! -d "/srv/www/default/webgrind" ]]; then
-    echo -e "\nDownloading webgrind, see https://github.com/jokkedk/webgrind"
-    git clone "https://github.com/jokkedk/webgrind.git" "/srv/www/default/webgrind"
+    echo -e "\nDownloading webgrind, see https://github.com/michaelschiller/webgrind.git"
+    git clone "https://github.com/michaelschiller/webgrind.git" "/srv/www/default/webgrind"
   else
     echo -e "\nUpdating webgrind..."
     cd /srv/www/default/webgrind
