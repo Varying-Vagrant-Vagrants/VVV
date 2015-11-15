7d3883abddb0dcdd2cf5ea986e966d4ade74e4de
Weston Ruter <weston@xwp.co>
2015-11-15 15:01:35 +1100

    Allow connections to wordpress_unit_tests database from host

--- database/init.sql
+++ database/init.sql
@@ -9,6 +9,7 @@ CREATE DATABASE IF NOT EXISTS `wordpress_develop`;
 GRANT ALL PRIVILEGES ON `wordpress_develop`.* TO 'wp'@'localhost' IDENTIFIED BY 'wp';
 CREATE DATABASE IF NOT EXISTS `wordpress_unit_tests`;
 GRANT ALL PRIVILEGES ON `wordpress_unit_tests`.* TO 'wp'@'localhost' IDENTIFIED BY 'wp';
+GRANT ALL PRIVILEGES ON `wordpress_unit_tests`.* TO 'wp'@'192.168.50.1' IDENTIFIED BY 'wp';
 
 # Create an external user with privileges on all databases in mysql so
 # that a connection can be made from the local machine without an SSH tunnel
