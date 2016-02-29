5f13f8c56a7c54f80c6d8d6c1108cd8b0be28723
Keegan Mullaney <keeganmullaney@gmail.com>
2016-02-29 22:20:56 +0800

    posible false positive in package_check
    use apt-cache policy instead of dpkg -s to check if pkg is NOT installed
    sometimes dpkg -s will find Version information for packages that have
    been removed, resulting in a false positive
    
     Changes to be committed:
    	modified:   provision/provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -159,15 +159,15 @@ package_check() {
   local package_version
 
   for pkg in "${apt_package_check_list[@]}"; do
-    package_version=$(dpkg -s "${pkg}" 2>&1 | grep 'Version:' | cut -d " " -f 2)
-    if [[ -n "${package_version}" ]]; then
+    if [[ -n "$(apt-cache policy ${pkg} | grep 'Installed: (none)')" ]]; then
+      echo " *" $pkg [not installed]
+      apt_package_install_list+=($pkg)
+    else
+      package_version=$(dpkg -s "${pkg}" 2>&1 | grep 'Version:' | cut -d " " -f 2)
       space_count="$(expr 20 - "${#pkg}")" #11
       pack_space_count="$(expr 30 - "${#package_version}")"
       real_space="$(expr ${space_count} + ${pack_space_count} + ${#package_version})"
       printf " * $pkg %${real_space}.${#package_version}s ${package_version}\n"
-    else
-      echo " *" $pkg [not installed]
-      apt_package_install_list+=($pkg)
     fi
   done
 }
