faf16ca9129de710a40b52944fad034e55bf87af
Jeremy Felt <jeremy.felt@gmail.com>
2016-08-15 23:43:36 -0700

    Remove WordPress trunk from default provisioning
    This has been around since the beginning of VVV, when the develop
    repos had not yet been created. There's not much sense in providing
    both the legacy trunk repo and the modern develop repo.
    
    We'll still want to document the best way to continue using the
    wordpress-trunk directory if it exists in your VVV instance.
    
    See #921.

--- provision/provision.sh
+++ provision/provision.sh
@@ -403,7 +403,7 @@ nginx_setup() {
             -key /etc/nginx/server.key \
             -out /etc/nginx/server.crt \
             -days 3650 \
-            -subj /CN=*.wordpress-develop.dev/CN=*.wordpress.dev/CN=*.vvv.dev/CN=*.wordpress-trunk.dev 2>&1)"
+            -subj /CN=*.wordpress-develop.dev/CN=*.wordpress.dev/CN=*.vvv.dev 2>&1)"
 	  echo "$vvvsigncert"
   fi
 
@@ -728,31 +728,6 @@ wpsvn_check() {
   fi;
 }
 
-wordpress_trunk() {
-  # Checkout, install and configure WordPress trunk via core.svn
-  if [[ ! -d "/srv/www/wordpress-trunk" ]]; then
-    echo "Checking out WordPress trunk from core.svn, see https://core.svn.wordpress.org/trunk"
-    svn checkout "https://core.svn.wordpress.org/trunk/" "/srv/www/wordpress-trunk"
-    cd /srv/www/wordpress-trunk
-    echo "Configuring WordPress trunk..."
-    noroot wp core config --dbname=wordpress_trunk --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-// Match any requests made via xip.io.
-if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(local.wordpress-trunk.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
-define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
-define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
-}
-
-define( 'WP_DEBUG', true );
-PHP
-    echo "Installing WordPress trunk..."
-    noroot wp core install --url=local.wordpress-trunk.dev --quiet --title="Local WordPress Trunk Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
-  else
-    echo "Updating WordPress trunk..."
-    cd /srv/www/wordpress-trunk
-    svn up
-  fi
-}
-
 wordpress_develop(){
   # Checkout, install and configure WordPress trunk via develop.svn
   if [[ ! -d "/srv/www/wordpress-develop" ]]; then
@@ -894,11 +869,10 @@ phpmyadmin_setup
 network_check
 # Time for WordPress!
 echo " "
-echo "Installing/updating WordPress Stable, Trunk & Develop"
+echo "Installing/updating WordPress Stable & Develop"
 
 wordpress_default
 wpsvn_check
-wordpress_trunk
 wordpress_develop
 
 # VVV custom site import
