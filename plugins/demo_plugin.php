89802a97947f8a21f837a1905362255cb616c63e
roelveldhuizen <roel@inesta.nl>
2016-08-30 12:18:26 +0200

    Added plugins directory to synced directories. Also for each file/directory a symlink is created in the WordPress instance and plugins are activated

---
+++ plugins/demo_plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Demo Plugin
+* Plugin URI: http://demoplugin.com/
+* Description: A brief description about your plugin.
+* Version: 1.0 or whatever version of the plugin (pretty self explanatory)
+* Author: Plugin Author's Name
+* Author URI: Author's website
+* License: A "Slug" license name e.g. GPL12
+*/
+
