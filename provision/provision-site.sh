f5dee3273d34f232f00b6307a521455299094ac7
LoreleiAurora <iam@l5i.me>
2017-01-17 23:38:08 +0000

    Update get_config_value to use gloval variable

--- provision/provision-site.sh
+++ provision/provision-site.sh
@@ -22,11 +22,7 @@ noroot() {
 # Takes 2 values, a key to fetch a value for, and an optional default value
 # e.g. echo `get_config_value 'key' 'defaultvalue'`
 get_config_value() {
-  local config=/vagrant/vvv-config.yml
-  if [[ -f /vagrant/vvv-custom.yml ]]; then
-    config=/vagrant/vvv-custom.yml
-  fi
-  local value=`cat ${config} | shyaml get-value sites.${SITE}.custom.${1} 2> /dev/null`
+  local value=`cat ${VVV_CONFIG} | shyaml get-value sites.${SITE}.custom.${1} 2> /dev/null`
   echo ${value:-$2}
 }
 
