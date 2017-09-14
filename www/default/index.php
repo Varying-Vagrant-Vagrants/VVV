c05eec9b7a3230638b6acdeec5c76a8d6458cb06
Tom J Nowell <contact@tomjn.com>
2017-09-14 14:02:17 -0700

    adds a search box for the documentation

--- www/default/index.php
+++ www/default/index.php
@@ -75,6 +75,13 @@ if ( file_exists( 'dashboard-custom.php' ) ) {
 	</div>
 	<div class="column">
 		<div class="box">
+			<h3>Search the Documentation</h3>
+			<form method="get" action="https://tomjn.github.io/varyingvagrantvagrants.org/search/" >
+				<input type="text" name="q" placeholder="search query"/>
+				<input type="submit" value="Search"/>
+			</form>
+		</div>
+		<div class="box">
 			<h3>Find out more about VVV</h3>
 			<a class="button" href="https://varyingvagrantvagrants.org/" target="_blank">Help &amp; Documentation</a>
 			<a class="button" href="https://github.com/varying-vagrant-vagrants/vvv/" target="_blank">View the code on GitHub</a>
