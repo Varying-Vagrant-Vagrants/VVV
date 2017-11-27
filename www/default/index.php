832e87ec041cc1a781580c6daf0cd1659168f1b6
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2017-11-27 15:42:10 +0530

    Fix typo

--- www/default/index.php
+++ www/default/index.php
@@ -155,7 +155,7 @@ require( __DIR__. '/dashboard/yaml.php' );
 		</div>
 		<div class="box">
 			<h3>Contribute to WordPress</h3>
-			<p>Wether you're at a contributor day, or just feel like giving back, you can add the WordPress.org Meta environment. This will give you everything from WordCamp to buddypress.org test sites</p>
+			<p>Whether you're at a contributor day, or just feel like giving back, you can add the WordPress.org Meta environment. This will give you everything from WordCamp to buddypress.org test sites</p>
 			<a class="button" href="https://github.com/WordPress/meta-environment">Find out more</a>
 		</div>
 		<div class="box">
