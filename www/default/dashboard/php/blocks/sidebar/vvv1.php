c76d1df03f9f4f8007a87fa4729002fb08ecb8ab
Tom J Nowell <contact@tomjn.com>
2018-02-19 20:30:41 +0000

    removes the dashboard folder in preparation for a git clone

--- www/default/dashboard/php/blocks/sidebar/vvv1.php
+++
@@ -1,5 +0,0 @@
-<div class="box">
-	<h3>VVV 1.x Sites not Showing?</h3>
-	<p>Sites need to be listed in <code>vvv-custom.yml</code> for VVV to find them, luckily it's super easy and fast to add them back! click below to find out how to migrate your sites.</p>
-	<a class="button" href="https://varyingvagrantvagrants.org/docs/en-US/adding-a-new-site/migrating-from-vvv-1-4-x/">Migrating VVV 1 sites</a>
-</div>
