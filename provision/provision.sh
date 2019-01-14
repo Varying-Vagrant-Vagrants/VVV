a7d32bf57f13aef55b950bdf9e46cc00878164f4
Anton Vanyukov <anton@incsub.com>
2019-01-14 12:39:41 +0300

    Remove MariaDB 10.1 in favor of 10.3

--- provision/provision.sh
+++ provision/provision.sh
@@ -211,8 +211,8 @@ package_install() {
   # Use debconf-set-selections to specify the default password for the root MariaDB
   # account. This runs on every provision, even if MariaDB has been installed. If
   # MariaDB is already installed, it will not affect anything.
-  echo mariadb-server-10.1 mysql-server/root_password password "root" | debconf-set-selections
-  echo mariadb-server-10.1 mysql-server/root_password_again password "root" | debconf-set-selections
+  echo mariadb-server-10.3 mysql-server/root_password password "root" | debconf-set-selections
+  echo mariadb-server-10.3 mysql-server/root_password_again password "root" | debconf-set-selections
 
   # Postfix
   #
