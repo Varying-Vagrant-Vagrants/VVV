74490a9c4789a019f62037e55ef00d6b00d0dcd9
Tom J Nowell <contact@tomjn.com>
2019-05-08 16:41:22 +0100

    document the running and not running of provisioners in the provisioner log

--- provision/provision-utility.sh
+++ provision/provision-utility.sh
@@ -2,5 +2,8 @@
 
 PROVISIONER="/srv/provision/resources/${1}/${2}/provision.sh"
 if [[ -f $PROVISIONER ]]; then
+	echo "Running utility provisioner for '${1}/${2}'"
     ${PROVISIONER}
+else
+	echo "Tried to run the utility provisioner for '${1}/${2}' but ${PROVISIONER} doesn't exist"
 fi
