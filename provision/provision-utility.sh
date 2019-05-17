8c52e93d41e909a5c2996ccdaef6e95b456b2155
Tom J Nowell <contact@tomjn.com>
2019-05-17 12:06:45 +0100

    logs utility provisioner output to a file

--- provision/provision-utility.sh
+++ provision/provision-utility.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+date_time=`cat /vagrant/provisioned_at`
+logfile="/var/log/provisioners/${date_time}/provisioner-utility-${1}-${2}.log"
+mkdir -p "${logfile}"
+touch "${logfile}"
+exec &> >(tee -a "${logfile}" >&2 )
+
 PROVISIONER="/srv/provision/utilities/${1}/${2}/provision.sh"
 
 if [[ -f $PROVISIONER ]]; then
