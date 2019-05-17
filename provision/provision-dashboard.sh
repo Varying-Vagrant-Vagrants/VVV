ff23438898db6d821c557d46b26dab577d67286f
Tom J Nowell <contact@tomjn.com>
2019-05-17 12:04:47 +0100

    logs the dashboard provisioner output to a file

--- provision/provision-dashboard.sh
+++ provision/provision-dashboard.sh
@@ -4,6 +4,12 @@ REPO=$1
 BRANCH=${2:-master}
 DIR="/srv/www/default/dashboard"
 
+date_time=`cat /vagrant/provisioned_at`
+logfile="/var/log/provisioners/${date_time}/provisioner-dashboard.log"
+mkdir -p "${logfile}"
+touch "${logfile}"
+exec &> >(tee -a "${logfile}" >&2 )
+
 if [[ false != "dashboard" && false != "${REPO}" ]]; then
   # Clone or pull the resources repository
   if [[ ! -d ${DIR}/.git ]]; then
