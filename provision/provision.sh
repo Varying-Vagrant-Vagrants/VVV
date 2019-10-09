cf51bab28d2748ff8079edb68364a7f330aa8530
Matias Saggiorato <matiassaggiorato@gmail.com>
2019-10-09 12:37:12 -0300

    Move "initial-setup" inline provisioner to provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -7,6 +7,35 @@
 # or `vagrant reload` are used. It provides all of the default packages and
 # configurations included with Varying Vagrant Vagrants.
 
+# cleanup
+mkdir -p /vagrant
+# change ownership for /vagrant folder
+sudo chown -R vagrant:vagrant /vagrant
+
+rm -f /vagrant/provisioned_at
+rm -f /vagrant/version
+rm -f /vagrant/vvv-custom.yml
+rm -f /vagrant/config.yml
+
+touch /vagrant/provisioned_at
+echo `date "+%Y%m%d-%H%M%S"` > /vagrant/provisioned_at
+
+# copy over version and config files
+cp -f /home/vagrant/version /vagrant
+cp -f /srv/config/config.yml /vagrant
+
+sudo chmod 0644 /vagrant/config.yml
+sudo chmod 0644 /vagrant/version
+sudo chmod 0644 /vagrant/provisioned_at
+
+# symlink the certificates folder for older site templates compat
+if [[ ! -d /vagrant/certificates ]]; then
+  ln -s /srv/certificates /vagrant/certificates
+fi
+
+# fix no tty warnings in provisioner logs
+sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile
+
 export DEBIAN_FRONTEND=noninteractive
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 export COMPOSER_ALLOW_SUPERUSER=1
