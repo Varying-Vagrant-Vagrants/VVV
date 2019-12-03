54351ccae1b6d7ac1413cd0d9b1b6d7ff53ca818
tomjn <contact@tomjn.com>
2019-12-03 20:54:55 +0000

    eliminate the need for exit statements

--- www/default/index.php
+++ www/default/index.php
@@ -6,13 +6,11 @@
  */
 if ( file_exists( 'dashboard-custom.php' ) ) {
 	include( 'dashboard-custom.php' );
-	exit;
-}
-
-if ( file_exists( 'dashboard/dashboard-default.php' ) ) {
-	include( 'dashboard/dashboard-default.php');
-	exit;
-}
-?>
+} else if ( !file_exists( 'dashboard/dashboard-default.php' ) ) {
+	?>
 <h2>VVV</h2>
 <p>VVV hasn't finished provisioning, and the dashboard hasn't been set up yet. Come back in a minute or two</p>
+	<?php
+} else {
+	include( 'dashboard/dashboard-default.php' );
+}
