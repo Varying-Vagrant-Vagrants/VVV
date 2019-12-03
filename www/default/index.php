6e6dc111472bc5dad9363e9456c8093d0c1f3e91
tomjn <contact@tomjn.com>
2019-12-03 20:59:55 +0000

    improve the dashboard bootstrap a little

--- www/default/index.php
+++ www/default/index.php
@@ -5,12 +5,12 @@
  * located in the `www/default/` directory.
  */
 if ( file_exists( 'dashboard-custom.php' ) ) {
-	include( 'dashboard-custom.php' );
-} else if ( !file_exists( 'dashboard/dashboard-default.php' ) ) {
+	include 'dashboard-custom.php';
+} else if ( file_exists( 'dashboard/dashboard-default.php' ) ) {
+	include 'dashboard/dashboard-default.php';
+} else{
 	?>
 <h2>VVV</h2>
 <p>VVV hasn't finished provisioning, and the dashboard hasn't been set up yet. Come back in a minute or two</p>
 	<?php
-} else {
-	include( 'dashboard/dashboard-default.php' );
 }
