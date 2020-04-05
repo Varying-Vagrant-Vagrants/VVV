90ac4dd72c5a001ae1623f3eba201719c2c9720f
tomjn <contact@tomjn.com>
2020-04-05 15:34:08 +0100

    move more of the site provisioner into functions and reorder how they're called

--- provision/provision-site.sh
+++ provision/provision-site.sh
@@ -15,6 +15,8 @@ RED="\033[38;5;9m"
 YELLOW="\033[38;5;3m"
 CRESET="\033[0m"
 
+SUCCESS=1
+
 # By storing the date now, we can calculate the duration of provisioning at the
 # end of this script.
 start_seconds="$(date +%s)"
@@ -29,28 +31,28 @@ exec 2> >(tee -a "${logfile}" >&2 )
 
 VVV_CONFIG=/vagrant/config.yml
 
-noroot() {
+function noroot() {
   sudo -EH -u "vagrant" "$@";
 }
 
 # Takes 2 values, a key to fetch a value for, and an optional default value
 # e.g. echo $(get_config_value 'key' 'defaultvalue')
-get_config_value() {
+function get_config_value() {
   local value=$(shyaml get-value "sites.${SITE_ESCAPED}.custom.${1}" 2> /dev/null < ${VVV_CONFIG})
   echo "${value:-$2}"
 }
 
-get_hosts() {
+function get_hosts() {
   local value=$(shyaml get-values-0 "sites.${SITE_ESCAPED}.hosts" 2> /dev/null < ${VVV_CONFIG} | tr '\0' ' ' | sed 's/ *$//')
   echo "${value:-$@}"
 }
 
-get_primary_host() {
+function get_primary_host() {
   local value=$(shyaml get-value "sites.${SITE_ESCAPED}.hosts.0" 2> /dev/null < ${VVV_CONFIG})
   echo "${value:-$1}"
 }
 
-function vvv_provision_site_nginx() {
+function vvv_provision_site_nginx_config() {
   SITE_NGINX_FILE=$2
   DEST_NGINX_FILE=${SITE_NGINX_FILE//\/srv\/www\//}
   DEST_NGINX_FILE=${DEST_NGINX_FILE//\//\-}
@@ -101,46 +103,77 @@ function vvv_provision_hosts_file() {
   done
 }
 
-if [[ true == "${SKIP_PROVISIONING}" ]]; then
-  echo -e "${YELLOW} * Skipping provisioning of ${SITE}${CRESET}"
-  return
-fi
-
-echo -e "${GREEN} * Running provisioner for site ${SITE}${CRESET}"
-
-if [[ false != "${REPO}" ]]; then
-  if [[ -d "${VM_DIR}" ]] && [[ ! -z "$(ls -A "${VM_DIR}")" ]]; then
-    if [[ -d "${VM_DIR}/.git" ]]; then
-    	echo " * Updating ${SITE} in ${VM_DIR}..."
-    	cd "${VM_DIR}"
-    	git reset "origin/${BRANCH}" --hard -q
-    	git pull origin "${BRANCH}" -q
-    	git checkout "${BRANCH}" -q
+function vvv_process_site_hosts() {
+  # Parse any vvv-hosts file located in the site repository for domains to
+  # be added to the virtual machine's host file so that it is self aware.
+  #
+  # Domains should be entered on new lines.
+  echo " * Adding domains to the virtual machine's /etc/hosts file..."
+  hosts=$(shyaml get-values "sites.${SITE_ESCAPED}.hosts" 2> /dev/null < ${VVV_CONFIG})
+  if [ ${#hosts[@]} -eq 0 ]; then
+    echo " * No hosts were found in the VVV config, falling back to vvv-hosts"
+    if [[ -f "${VM_DIR}/.vvv/vvv-hosts" ]]; then
+      echo -e "${GREEN} * Found a .vvv/vvv-hosts file${CRESET}"
+      vvv_provision_hosts_file "${SITE}" "${VM_DIR}/.vvv/vvv-hosts"
+    elif [[ -f "${VM_DIR}/provision/vvv-hosts" ]]; then
+      echo -e "${GREEN} * Found a provision/vvv-hosts file${CRESET}"
+      vvv_provision_hosts_file "${SITE}" "${VM_DIR}/provision/vvv-hosts"
+    elif [[ -f "${VM_DIR}/vvv-hosts" ]]; then
+      echo -e "${GREEN} * Found a vvv-hosts file${CRESET}"
+      vvv_provision_hosts_file "${SITE}" "${VM_DIR}/vvv-hosts"
     else
-      echo "${RED}Problem! A site folder for ${SITE} was found at ${VM_DIR} that doesn't use a site template, but a site template is defined in the config file. Either the config file is mistaken, or a previous attempt to provision has failed, VVV will not try to git clone the site template to avoid data destruction, either remove the folder, or fix the config/config.yml entry${CRESET}"
+      echo " * Searching subfolders 4 levels down for a vvv-hosts file ( this can be skipped by using ./vvv-hosts, .vvv/vvv-hosts, or provision/vvv-hosts"
+      HOST_FILES=$(find "${VM_DIR}" -maxdepth 4 -name 'vvv-hosts');
+      if [[ -z $HOST_FILES ]] ; then
+        echo -e "${RED} ! Warning: No vvv-hosts file was found, and no hosts were defined in the vvv config, this site may be inaccessible${CRESET}"
+      else
+        for HOST_FILE in $HOST_FILES; do
+          vvv_provision_hosts_file "$HOST_FILE"
+        done
+      fi
     fi
   else
-    # Clone or pull the site repository
-    echo -e " * Downloading ${SITE}, git cloning from ${REPO} into ${VM_DIR}"
-    git clone --recursive --branch "${BRANCH}" "${REPO}" "${VM_DIR}" -q
-    if [ $? -eq 0 ]; then
-      echo " * ${SITE} Site Template clone succesful"
+    echo " * Adding hosts from the VVV config entry"
+    for line in $(shyaml get-values "sites.${SITE_ESCAPED}.hosts" 2> /dev/null < ${VVV_CONFIG}); do
+      if [[ -z "$(grep -q "^127.0.0.1 $line$" /etc/hosts)" ]]; then
+        echo "127.0.0.1 ${line} # vvv-auto" >> "/etc/hosts"
+        echo "   - Added ${line} from ${VVV_CONFIG}"
+      fi
+    done
+  fi
+}
+
+function vvv_provision_site_repo() {
+  if [[ false != "${REPO}" ]]; then
+    if [[ -d "${VM_DIR}" ]] && [[ ! -z "$(ls -A "${VM_DIR}")" ]]; then
+      if [[ -d "${VM_DIR}/.git" ]]; then
+        echo " * Updating ${SITE} in ${VM_DIR}..."
+        cd "${VM_DIR}"
+        git reset "origin/${BRANCH}" --hard -q
+        git pull origin "${BRANCH}" -q
+        git checkout "${BRANCH}" -q
+      else
+        echo "${RED}Problem! A site folder for ${SITE} was found at ${VM_DIR} that doesn't use a site template, but a site template is defined in the config file. Either the config file is mistaken, or a previous attempt to provision has failed, VVV will not try to git clone the site template to avoid data destruction, either remove the folder, or fix the config/config.yml entry${CRESET}"
+      fi
     else
-      echo "${RED} ! Git failed to clone the site template for ${SITE}. It tried to clone the ${BRANCH} of ${REPO} into ${VM_DIR}${CRESET}"
-      echo "${RED} ! VVV won't be able to provision ${SITE} without the template. Check that you have permission to access the repo, and that the filesystem is writable${CRESET}"
-      exit 1
+      # Clone or pull the site repository
+      echo -e " * Downloading ${SITE}, git cloning from ${REPO} into ${VM_DIR}"
+      git clone --recursive --branch "${BRANCH}" "${REPO}" "${VM_DIR}" -q
+      if [ $? -eq 0 ]; then
+        echo " * ${SITE} Site Template clone succesful"
+      else
+        echo "${RED} ! Git failed to clone the site template for ${SITE}. It tried to clone the ${BRANCH} of ${REPO} into ${VM_DIR}${CRESET}"
+        echo "${RED} ! VVV won't be able to provision ${SITE} without the template. Check that you have permission to access the repo, and that the filesystem is writable${CRESET}"
+        exit 1
+      fi
+    fi
+  else
+    echo " * The site: '${SITE}' does not have a site template, assuming custom provision/vvv-init.sh and provision/vvv-nginx.conf"
+    if [[ ! -d "${VM_DIR}" ]]; then
+      echo "${RED} ! Error: The '${SITE}' has no folder, VVV does not create the folder for you, or set up the Nginx configs. Use a site template or create the folder and provisioner files, then reprovision VVV${CRESET}"
     fi
   fi
-else
-  echo " * The site: '${SITE}' does not have a site template, assuming custom provision/vvv-init.sh and provision/vvv-nginx.conf"
-  if [[ ! -d "${VM_DIR}" ]]; then
-    echo "${RED} ! Error: The '${SITE}' has no folder, VVV does not create the folder for you, or set up the Nginx configs. Use a site template or create the folder and provisioner files, then reprovision VVV${CRESET}"
-  fi
-fi
-
-if [[ ! -d "${VM_DIR}" ]]; then
-  echo "${RED} ! Error: The ${VM_DIR} folder does not exist, there is nothing to provision for the '${SITE}' site! ${CRESET}"
-fi
+}
 
 function vvv_run_site_template_script() {
   echo " * Found ${1} at ${2}/${1}"
@@ -155,87 +188,71 @@ function vvv_run_site_template_script() {
   fi
 }
 
-SUCCESS=1
-# Look for site setup scripts
-echo " * Searching for a site template provisioner, vvv-init.sh"
-if [[ -f "${VM_DIR}/.vvv/vvv-init.sh" ]]; then
-  vvv_run_site_template_script "vvv-init.sh" "${VM_DIR}/.vvv"
-  SUCCESS=$?
-elif [[ -f "${VM_DIR}/provision/vvv-init.sh" ]]; then
-  vvv_run_site_template_script "vvv-init.sh" "${VM_DIR}/provision"
-  SUCCESS=$?
-elif [[ -f "${VM_DIR}/vvv-init.sh" ]]; then
-  vvv_run_site_template_script "vvv-init.sh" "${VM_DIR}"
-  SUCCESS=$?
-else
-  echo " * Warning: A site provisioner was not found at .vvv/vvv-init.conf provision/vvv-init.conf or vvv-init.conf, searching 3 folders down, please be patient..."
-  SITE_INIT_SCRIPTS=$(find "${VM_DIR}" -maxdepth 3 -name 'vvv-init.conf');
-  if [[ -z $SITE_INIT_SCRIPTS ]] ; then
-    echo " * Warning: No site provisioner was found, VVV could not perform any scripted setup that might install software for this site"
+function vvv_provision_site_script() {
+  # Look for site setup scripts
+  echo " * Searching for a site template provisioner, vvv-init.sh"
+  if [[ -f "${VM_DIR}/.vvv/vvv-init.sh" ]]; then
+    vvv_run_site_template_script "vvv-init.sh" "${VM_DIR}/.vvv"
+    SUCCESS=$?
+  elif [[ -f "${VM_DIR}/provision/vvv-init.sh" ]]; then
+    vvv_run_site_template_script "vvv-init.sh" "${VM_DIR}/provision"
+    SUCCESS=$?
+  elif [[ -f "${VM_DIR}/vvv-init.sh" ]]; then
+    vvv_run_site_template_script "vvv-init.sh" "${VM_DIR}"
+    SUCCESS=$?
   else
-    for SITE_INIT_SCRIPT in $SITE_INIT_SCRIPTS; do
-      DIR="$(dirname "$SITE_INIT_SCRIPT")"
-      vvv_run_site_template_script "vvv-init.sh" "${DIR}"
-    done
-  fi
-fi
-
-# Look for Nginx vhost files, symlink them into the custom sites dir
-if [[ -f "${VM_DIR}/.vvv/vvv-nginx.conf" ]]; then
-  vvv_provision_site_nginx "${SITE}" "${VM_DIR}/.vvv/vvv-nginx.conf"
-elif [[ -f "${VM_DIR}/provision/vvv-nginx.conf" ]]; then
-  vvv_provision_site_nginx "${SITE}" "${VM_DIR}/provision/vvv-nginx.conf"
-elif [[ -f "${VM_DIR}/vvv-nginx.conf" ]]; then
-  vvv_provision_site_nginx "${SITE}" "${VM_DIR}/vvv-nginx.conf"
-else
-  echo " * Warning: An nginx config was not found at .vvv/vvv-nginx.conf provision/vvv-nginx.conf or vvv-nginx.conf, searching 3 folders down, please be patient..."
-  NGINX_CONFIGS=$(find "${VM_DIR}" -maxdepth 3 -name 'vvv-nginx.conf');
-  if [[ -z $NGINX_CONFIGS ]] ; then
-    echo -e "${RED} ! Warning: No nginx config was found, VVV will not know how to serve this site${CRESET}"
-  else
-    for SITE_CONFIG_FILE in $NGINX_CONFIGS; do
-      vvv_provision_site_nginx "${SITE}" "${SITE_CONFIG_FILE}"
-    done
-  fi
-fi
-
-# Parse any vvv-hosts file located in the site repository for domains to
-# be added to the virtual machine's host file so that it is self aware.
-#
-# Domains should be entered on new lines.
-echo " * Adding domains to the virtual machine's /etc/hosts file..."
-hosts=$(shyaml get-values "sites.${SITE_ESCAPED}.hosts" 2> /dev/null < ${VVV_CONFIG})
-if [ ${#hosts[@]} -eq 0 ]; then
-  echo " * No hosts were found in the VVV config, falling back to vvv-hosts"
-  if [[ -f "${VM_DIR}/.vvv/vvv-hosts" ]]; then
-    echo -e "${GREEN} * Found a .vvv/vvv-hosts file${CRESET}"
-    vvv_provision_hosts_file "${SITE}" "${VM_DIR}/.vvv/vvv-hosts"
-  elif [[ -f "${VM_DIR}/provision/vvv-hosts" ]]; then
-    echo -e "${GREEN} * Found a provision/vvv-hosts file${CRESET}"
-    vvv_provision_hosts_file "${SITE}" "${VM_DIR}/provision/vvv-hosts"
-  elif [[ -f "${VM_DIR}/vvv-hosts" ]]; then
-    echo -e "${GREEN} * Found a vvv-hosts file${CRESET}"
-    vvv_provision_hosts_file "${SITE}" "${VM_DIR}/vvv-hosts"
-  else
-    echo " * Searching subfolders 4 levels down for a vvv-hosts file ( this can be skipped by using ./vvv-hosts, .vvv/vvv-hosts, or provision/vvv-hosts"
-    HOST_FILES=$(find "${VM_DIR}" -maxdepth 4 -name 'vvv-hosts');
-    if [[ -z $HOST_FILES ]] ; then
-      echo -e "${RED} ! Warning: No vvv-hosts file was found, and no hosts were defined in the vvv config, this site may be inaccessible${CRESET}"
+    echo " * Warning: A site provisioner was not found at .vvv/vvv-init.conf provision/vvv-init.conf or vvv-init.conf, searching 3 folders down, please be patient..."
+    SITE_INIT_SCRIPTS=$(find "${VM_DIR}" -maxdepth 3 -name 'vvv-init.conf');
+    if [[ -z $SITE_INIT_SCRIPTS ]] ; then
+      echo " * Warning: No site provisioner was found, VVV could not perform any scripted setup that might install software for this site"
     else
-      for HOST_FILE in $HOST_FILES; do
-        vvv_provision_hosts_file "$HOST_FILE"
+      for SITE_INIT_SCRIPT in $SITE_INIT_SCRIPTS; do
+        DIR="$(dirname "$SITE_INIT_SCRIPT")"
+        vvv_run_site_template_script "vvv-init.sh" "${DIR}"
       done
     fi
   fi
-else
-  echo " * Adding hosts from the VVV config entry"
-  for line in $(shyaml get-values "sites.${SITE_ESCAPED}.hosts" 2> /dev/null < ${VVV_CONFIG}); do
-    if [[ -z "$(grep -q "^127.0.0.1 $line$" /etc/hosts)" ]]; then
-      echo "127.0.0.1 ${line} # vvv-auto" >> "/etc/hosts"
-      echo "   - Added ${line} from ${VVV_CONFIG}"
+}
+
+function vvv_provision_site_nginx() {
+  # Look for Nginx vhost files, symlink them into the custom sites dir
+  if [[ -f "${VM_DIR}/.vvv/vvv-nginx.conf" ]]; then
+    vvv_provision_site_nginx_config "${SITE}" "${VM_DIR}/.vvv/vvv-nginx.conf"
+  elif [[ -f "${VM_DIR}/provision/vvv-nginx.conf" ]]; then
+    vvv_provision_site_nginx_config "${SITE}" "${VM_DIR}/provision/vvv-nginx.conf"
+  elif [[ -f "${VM_DIR}/vvv-nginx.conf" ]]; then
+    vvv_provision_site_nginx_config "${SITE}" "${VM_DIR}/vvv-nginx.conf"
+  else
+    echo " * Warning: An nginx config was not found at .vvv/vvv-nginx.conf provision/vvv-nginx.conf or vvv-nginx.conf, searching 3 folders down, please be patient..."
+    NGINX_CONFIGS=$(find "${VM_DIR}" -maxdepth 3 -name 'vvv-nginx.conf');
+    if [[ -z $NGINX_CONFIGS ]] ; then
+      echo -e "${RED} ! Warning: No nginx config was found, VVV will not know how to serve this site${CRESET}"
+    else
+      for SITE_CONFIG_FILE in $NGINX_CONFIGS; do
+        vvv_provision_site_nginx_config "${SITE}" "${SITE_CONFIG_FILE}"
+      done
     fi
-  done
+  fi
+}
+
+# -------------------------------
+
+if [[ true == "${SKIP_PROVISIONING}" ]]; then
+  echo -e "${YELLOW} * Skipping provisioning of ${SITE}${CRESET}"
+  return 0
 fi
+
+echo -e "${GREEN} * Running provisioner for site ${SITE}${CRESET}"
+
+vvv_provision_site_repo
+
+if [[ ! -d "${VM_DIR}" ]]; then
+  echo "${RED} ! Error: The ${VM_DIR} folder does not exist, there is nothing to provision for the '${SITE}' site! ${CRESET}"
+fi
+
+vvv_process_site_hosts
+vvv_provision_site_script
+vvv_provision_site_nginx
 
 echo " * Reloading Nginx"
 service nginx reload
