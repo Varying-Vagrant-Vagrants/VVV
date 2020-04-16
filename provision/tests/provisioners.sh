77e233473dc15cc1b9b499d9256e916d0a7bc1bb
Matias Saggiorato <matiassaggiorato@gmail.com>
2020-04-16 13:33:42 -0300

    Create provision_site function

--- provision/tests/provisioners.sh
+++ provision/tests/provisioners.sh
@@ -106,23 +106,28 @@ function provision_sites() {
   local sites=($(get_config_keys sites))
   local site
   for site in ${sites[@]}; do
-    local skip_provisioning=$(get_config_value "sites.${site}.skip_provisioning" "False")
-    if [[ $skip_provisioning == "True" ]]; then
-      continue
-    fi
-    local repo=$(get_config_type "sites.${site}")
-    if [[ "${repo}" == "str" ]]; then
-      repo=$(get_config_value "sites.${site}" "")
-    else
-      repo=$(get_config_value "sites.${site}.repo" "")
-    fi
-    local branch=$(get_config_value "sites.${site}.branch" "master")
-    local vm_dir=$(get_config_value "sites.${site}.vm_dir" "/srv/www/${site}")
-    local nginx_upstream=$(get_config_value "sites.${site}.nginx_upstream" "php")
-
-    VVV_LOG="site-${site}"
-    bash /srv/provision/provision-site.sh "${site}" "${repo}" "${branch}" "${vm_dir}" "${skip_provisioning}" "${nginx_upstream}"
+    provision_site "${site}"
   done
+}
+
+function provision_site() {
+  local site=$1
+  local skip_provisioning=$(get_config_value "sites.${site}.skip_provisioning" "False")
+  if [[ $skip_provisioning == "True" ]]; then
+    continue
+  fi
+  local repo=$(get_config_type "sites.${site}")
+  if [[ "${repo}" == "str" ]]; then
+    repo=$(get_config_value "sites.${site}" "")
+  else
+    repo=$(get_config_value "sites.${site}.repo" "")
+  fi
+  local branch=$(get_config_value "sites.${site}.branch" "master")
+  local vm_dir=$(get_config_value "sites.${site}.vm_dir" "/srv/www/${site}")
+  local nginx_upstream=$(get_config_value "sites.${site}.nginx_upstream" "php")
+
+  VVV_LOG="site-${site}"
+  bash /srv/provision/provision-site.sh "${site}" "${repo}" "${branch}" "${vm_dir}" "${skip_provisioning}" "${nginx_upstream}"
 }
 
 function provision_main() {
