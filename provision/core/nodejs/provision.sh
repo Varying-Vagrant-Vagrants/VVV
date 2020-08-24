56051b61d9592cb44f87bde3b4780ddf1bacc4f5
Matias Saggiorato <matiassaggiorato@gmail.com>
2020-08-24 11:41:23 -0300

    Setup node after packages are installed

--- provision/core/nodejs/provision.sh
+++ provision/core/nodejs/provision.sh
@@ -20,3 +20,22 @@ VVV_PACKAGE_LIST+=(
   g++
   nodejs
 )
+
+node_setup() {
+  if [[ $(nodejs -v | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*/\1/p') != '10' ]]; then
+    echo " * Downgrading to Node v10."
+    apt remove nodejs -y
+    apt install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install --fix-missing --fix-broken nodejs
+  fi
+
+  # npm
+  #
+  # Make sure we have the latest npm version and the update checker module
+  echo " * Installing/updating npm..."
+  npm_config_loglevel=error npm install -g npm
+  echo " * Installing/updating npm-check-updates..."
+  npm_config_loglevel=error npm install -g npm-check-updates
+}
+export -f node_setup
+
+vvv_add_hook after_packages node_setup
--- provision/provision.sh
+++ provision/provision.sh
@@ -108,20 +108,6 @@ tools_install() {
   # Disable xdebug before any composer provisioning.
   sh /srv/config/homebin/xdebug_off
 
-  if [[ $(nodejs -v | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*/\1/p') != '10' ]]; then
-    echo " * Downgrading to Node v10."
-    apt remove nodejs -y
-    apt install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install --fix-missing --fix-broken nodejs
-  fi
-
-  # npm
-  #
-  # Make sure we have the latest npm version and the update checker module
-  echo " * Installing/updating npm..."
-  npm_config_loglevel=error npm install -g npm
-  echo " * Installing/updating npm-check-updates..."
-  npm_config_loglevel=error npm install -g npm-check-updates
-
   echo " * Making sure the composer cache is not owned by root"
   mkdir -p /usr/local/src/composer
   mkdir -p /usr/local/src/composer/cache
@@ -569,6 +555,8 @@ if ! package_install; then
   vvv_error " ! Main packages check and install failed, halting provision"
   exit 1
 fi
+
+vvv_hook after_packages
 
 tools_install
 
