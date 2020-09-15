e4dac84f0b91a54c0a58a5e37ac47db84552eb61
Tom J Nowell <contact@tomjn.com>
2020-09-15 16:32:30 +0100

    add noroot to dashboard provisioner
    Also adjust git clone to match site template

--- provision/provision-dashboard.sh
+++ provision/provision-dashboard.sh
@@ -7,21 +7,23 @@ BRANCH=${2:-master}
 DIR="/srv/www/default/dashboard"
 
 if [[ false != "dashboard" && false != "${REPO}" ]]; then
+  noroot mkdir -p "${DIR}"
   # Clone or pull the resources repository
   if [[ ! -d "${DIR}/.git" ]]; then
-    echo -e " *  Downloading dashboard, see ${REPO}"
-    git clone "${REPO}" --branch "${BRANCH}" "${DIR}" -q
+    echo -e " *  Cloning dashboard from '${REPO}' into '${DIR}' using the '${BRANCH}' branch."
+    noroot git clone "${REPO}" --branch "${BRANCH}" "${DIR}" -q
     cd "${DIR}"
-    git checkout "${BRANCH}" -q
+    noroot git checkout "${BRANCH}" -q
   else
-    echo -e " * Updating dashboard on the '${BRANCH}' branch..."
+    echo -e " * Updating dashboard on the '${BRANCH}' branch."
     cd "${DIR}"
-    git pull origin "${BRANCH}" -q
-    git checkout "${BRANCH}" -q
+    noroot git reset "origin/${BRANCH}" --hard -q
+    noroot git pull origin "${BRANCH}" -q
+    noroot git checkout "${BRANCH}" -q
   fi
+  echo " * Note that custom dashboads will be going away in a future update, use a site provisioner and a custom host instead such as dashboard.test."
 else
-  echo " * Skipping dashboard provisioning, set to false in config"
+  echo " * Skipping dashboard provisioning, dashboard repository was set to false in config.yml"
 fi
 
 provisioner_success
-
