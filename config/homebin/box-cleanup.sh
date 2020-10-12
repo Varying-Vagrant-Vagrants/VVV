ceacad91edb7006674055d0f3e9d1f7ed9df230d
Tom J Nowell <contact@tomjn.com>
2020-10-12 17:35:29 +0100

    fix a globbing warning in the box cleanup find commands

--- config/homebin/box-cleanup.sh
+++ config/homebin/box-cleanup.sh
@@ -67,7 +67,7 @@ rm -rf /usr/share/doc/*
 find /var/cache -type f -exec rm -rf {} \;
 
 # delete any logs that have built up during the install
-find /var/log/ -name *.log -exec rm -f {} \;
+find /var/log/ -name "*.log" -exec rm -f {} \;
 
 # Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
 truncate -s 0 /etc/machine-id
