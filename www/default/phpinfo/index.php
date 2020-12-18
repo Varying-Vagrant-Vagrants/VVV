d23f588391226b5ea28d185ef808e349f23c3f3c
tomjn <contact@tomjn.com>
2020-12-18 16:57:43 +0000

    add xdebug info to the phpinnfo page if it's available

--- www/default/phpinfo/index.php
+++ www/default/phpinfo/index.php
@@ -2,5 +2,23 @@
 /**
  * Output the phpinfo for our setup
  */
+?>
+<nav class="center">
+	<a href="#phpinfo">PHP Info</a>
+	<?php
+	if ( function_exists( 'xdebug_info' ) ) {
+		?>, <a href="#xdebuginfo">XDebug Info</a><?php
+	}
+	?>
+</nav>
+<hr/>
+<?php
+echo '<div id="phpinfo">';
 phpinfo();
+echo '</div>';
 
+if ( function_exists( 'xdebug_info' ) ) {
+	echo '<hr/><div id="xdebuginfo">';
+	xdebug_info();
+	echo '</div>';
+}
