0ac2ac936071437b2c0977285621ff18a0d07bb5
tomjn <contact@tomjn.com>
2020-12-24 19:10:44 +0000

    tweak the deprecated distro message

--- provision/core/deprecated.sh
+++ provision/core/deprecated.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-# @descriptionn This file contains deprecated functions that may be removed later.
+#!/usr/bin/env bash
+# @description This file contains deprecated functions that may be removed later.
 
 # @description Check if we're on Ubuntu 14 and abort provisioning
+# @noargs
 function deprecated_distro() {
   local command_exist
   command_exist="$(which lsb_release)"
@@ -11,62 +12,63 @@ function deprecated_distro() {
   codename=$(lsb_release --codename | cut -f2)
   if [[ $codename == "trusty" ]]; then
     r="\e[0;32m"
-    echo " "
-    echo '__ __ __ __'
-    echo '\ V\ V\ V / A message from the VVV team'
-    echo ' \_/\_/\_/  '
-    echo -e " "
-    echo -e "We Have Some Good News and Some Bad News"
-    echo -e "----------------------------------------"
-    echo -e " "
-    echo -e "The good news is that you updated to VVV 3+! Thanks for taking "
-    echo -e "care of your install!"
-    echo -e " "
-    echo -e "The bad news is that your VM is still an Ubuntu 14 VM. VVV 3+ needs"
-    echo -e "Ubuntu 18, and requires a vagant destroy and a reprovision."
-    echo -e " "
-    echo -e " "
-    echo -e "\e[1;4;33mImportant: Destroying and reprovisioning will erase the database${r}"
-    echo -e " "
+    vvv_error " "
+    vvv_error '__ __ __ __'
+    vvv_error '\ V\ V\ V / A message from the VVV team'
+    vvv_error ' \_/\_/\_/  '
+    vvv_error " "
+    vvv_error "We Have Some Good News and Some Bad News"
+    vvv_error "----------------------------------------"
+    vvv_error " "
+    vvv_error "The good news is that you updated to VVV 3+! Thanks for taking "
+    vvv_error "care of your install!"
+    vvv_error " "
+    vvv_error "The bad news is that your VM is still an Ubuntu 14 VM. VVV 3+ needs"
+    vvv_error "Ubuntu 18, and requires a DB backup, then a vagant destroy,"
+    vvv_error "a reprovision, then a database restore."
+    vvv_error " "
+    vvv_error " "
+    vvv_error "<b>Important: Destroying and reprovisioning will erase the database</b>"
+    vvv_error " "
     sqlcount=$(cd /srv/database/backups; ls -1q ./*.sql | wc -l)
     if [[ $sqlcount -gt 0 ]]; then
-      echo -e "\e[0;33m "
-      echo -e "\e[0;33mLuckily, VVV backs up the database to database/backups, and "
-      echo -e "we found ${sqlcount} of those .sql files in database/backups from the last "
-      echo -e "time you ran vagrant halt."
+      vvv_error " "
+      vvv_error "Luckily, VVV backs up the database to database/backups, and "
+      vvv_error "we found ${sqlcount} of those .sql files in database/backups from the last "
+      vvv_error "time you ran vagrant halt."
 
-      echo -e "These DB backups can be restored after updating with this command:"
-      echo -e "vagrant ssh -c \"db_restore\"${r}"
+      vvv_error "These DB backups can be restored after updating with this command:"
+      vvv_error "vagrant ssh -c \"db_restore\""
     else
-      echo -e "\e[0;33m "
-      echo -e "\e[0;33mNormally VVV takes backups, but we didn't find any existing backups${r}"
-      echo -e " "
-      echo -e "How Do I Grab Database Backups?"
-      echo -e "--------------------------------"
-      echo -e " "
-      echo -e "If you've turned off database backups, or haven't turned off your VM"
-      echo -e "in a while, take the following steps:"
-      echo -e " "
-      echo -e " 1. downgrade back to VVV 2:             git fetch --tags && git checkout 2.6.0"
-      echo -e " 2. turn on the VM but don't provision:  vagrant up"
-      echo -e " 3. run the backup DB script:            vagrant ssh -c \"db_backup\""
-      echo -e " 4. turn off the VM:                     vagrant halt"
-      echo -e " 5. return to VVV 3+:                    git checkout develop"
-      echo -e " 6. you can now update your VM to VVV 3, see the instructions in the section above"
+      vvv_error " "
+      vvv_error "Normally VVV takes backups, but we didn't find any existing backups"
+      vvv_error " "
+      vvv_error "How Do I Grab Database Backups?"
+      vvv_error "--------------------------------"
+      vvv_error " "
+      vvv_error "If you've turned off database backups, or haven't turned off your VM"
+      vvv_error "in a while, take the following steps:"
+      vvv_error " "
+      vvv_error " 1. downgrade back to VVV 2:             git fetch --tags && git checkout 2.6.0"
+      vvv_error " 2. turn on the VM but don't provision:  vagrant up"
+      vvv_error " 3. run the backup DB script:            vagrant ssh -c \"db_backup\""
+      vvv_error " 4. turn off the VM:                     vagrant halt"
+      vvv_error " 5. return to VVV 3+:                    git checkout develop"
+      vvv_error " 6. you can now update your VM to VVV 3, see the instructions in the section above"
     fi
-    echo -e " "
-    echo -e " "
-    echo -e "Updating Your VM To VVV 3+"
-    echo -e "--------------------------"
-    echo -e " "
-    echo -e "If you're happy and have your database files, you can update your VM to VVV 3+ with these commands:"
-    echo -e " "
-    echo -e " 1. destroy the VM:              vagrant destroy"
-    echo -e " 2. provision a new VM:          vagrant up --provision"
-    echo -e " 3. optionally restore backups:  vagrant ssh -c \"db_restore\""
-    echo -e " "
-    echo -e " "
-    echo -e " "
+    vvv_error " "
+    vvv_error " "
+    vvv_error "Updating Your VM To VVV 3+"
+    vvv_error "--------------------------"
+    vvv_error " "
+    vvv_error "If you're happy and have your database files, you can update your VM to VVV 3+ with these commands:"
+    vvv_error " "
+    vvv_error " 1. destroy the VM:              vagrant destroy"
+    vvv_error " 2. provision a new VM:          vagrant up --provision"
+    vvv_error " 3. optionally restore backups:  vagrant ssh -c \"db_restore\""
+    vvv_error " "
+    vvv_error " "
+    vvv_error " "
     exit 1
   fi
 }
