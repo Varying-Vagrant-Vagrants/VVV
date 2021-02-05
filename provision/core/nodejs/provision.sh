a648cb914c2ade72f3daa70f924271af1e5d5431
tomjn <contact@tomjn.com>
2021-02-05 00:26:48 +0000

    if /usr/bin/npm does not exist, purge and reinstall nodejs

--- provision/core/nodejs/provision.sh
+++ provision/core/nodejs/provision.sh
@@ -19,17 +19,26 @@ function nodejs_register_packages() {
 }
 vvv_add_hook before_packages nodejs_register_packages
 
+function reinstall_node() {
+  vvv_info " * Purging NodeJS package."
+  apt-get purge nodejs -y
+  vvv_info " * Cleaning apt."
+  apt-get clean -y
+  vvv_info " * Apt autoremove."
+  apt-get autoremove -y
+  vvv_info " * Installing Node 14 LTS."
+  apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install --fix-missing --fix-broken nodejs
+  vvv_success " ✓ Reinstalled Node, if you need another version use the nvm utility"
+}
+
 function node_setup() {
   if [[ $(nodejs -v | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*/\1/p') != '14' ]]; then
     vvv_info " * Migrating to Node v14."
-    vvv_info " * Purging NodeJS package."
-    apt-get purge nodejs -y
-    vvv_info " * Cleaning apt."
-    apt-get clean -y
-    apt-get autoremove -y
-    vvv_info " * Installing Node 14 LTS."
-    apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install --fix-missing --fix-broken nodejs
-    vvv_success " ✓ Reinstalled Node, if you need another version use the nvm utility"
+    reinstall_node
+  fi
+  if [[ ! -f "/usr/bin/npm" ]]; then
+    vvv_warn " ! npm is missing in /usr/bin, reinstalling Node"
+    reinstall_node
   fi
 }
 export -f node_setup
