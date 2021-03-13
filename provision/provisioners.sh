f77690b4deb9d57cc30d3af2ad0d31f8c936d9e8
Tom J Nowell <contact@tomjn.com>
2021-03-13 18:36:17 +0000

    fix an infinite loop when provisioners fail

--- provision/provisioners.sh
+++ provision/provisioners.sh
@@ -35,10 +35,7 @@ function provisioner_end() {
     vvv_success " ✔ The <b>'${VVV_PROVISIONER_RUNNING}'</b><success> provisioner completed in </success><b>${elapsed}</b><success> seconds.</success>"
     rm -f "/vagrant/failed_provisioners/provisioner-${VVV_PROVISIONER_RUNNING}"
   else
-    vvv_error " ! The <b>'${VVV_PROVISIONER_RUNNING}'</b><error> provisioner ran into problems, printing full log:"
-    cat "${VVV_CURRENT_LOG_FILE}"
-    vvv_error " ! The <b>'${VVV_PROVISIONER_RUNNING}'</b><error> provisioner ran into problems, the full log is printed above. It completed in <b>${elapsed}</b><error> seconds."
-    
+    vvv_error " ! The <b>'${VVV_PROVISIONER_RUNNING}'</b><error> provisioner ran into problems, the full log is available at <b>'${VVV_CURRENT_LOG_FILE}'</b><error>. It completed in <b>${elapsed}</b><error> seconds."
   fi
   trap - EXIT
 }
