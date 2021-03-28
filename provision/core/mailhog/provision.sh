1f7af26203ad4391728d9c1280d5a7df5e635df3
Dan Griffiths <dgriffiths@widgit.io>
2021-03-28 17:56:01 -0400

    Add ARM64 support for Mailhog

--- provision/core/mailhog/provision.sh
+++ provision/core/mailhog/provision.sh
@@ -10,22 +10,21 @@ function mailhog_setup() {
 
   if [[ ! -e /usr/local/bin/mailhog ]]; then
     vvv_info " * Installing MailHog"
-
+    mailhog_bin="https://github.com/mailhog/MailHog/releases/download/v1.0.0/MailHog_linux_amd64"
     if [[ "aarch64" == $(uname -m) ]]; then
-      curl --silent -L -o /usr/local/bin/mailhog https://github.com/evertiro/MailHog/releases/download/v1.0.0-M1/MailHog_linux_arm64
-    else
-      curl --silent -L -o /usr/local/bin/mailhog https://github.com/mailhog/MailHog/releases/download/v1.0.0/MailHog_linux_amd64
+      mailhog_bin="https://github.com/evertiro/MailHog/releases/download/v1.0.0-M1/MailHog_linux_arm64"
     fi
+    curl --silent -L -o /usr/local/bin/mailhog ${mailhog_bin}
     chmod +x /usr/local/bin/mailhog
     vvv_success " * Mailhog binary installed"
   fi
   if [[ ! -e /usr/local/bin/mhsendmail ]]; then
     vvv_info " * Installing MHSendmail"
+    mhsendmail_bin="https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64"
     if [[ "aarch64" == $(uname -m) ]]; then
-      curl --silent -L -o /usr/local/bin/mhsendmail https://github.com/evertiro/mhsendmail/releases/download/v0.2.0-M1/mhsendmail_linux_arm64
-    else
-      curl --silent -L -o /usr/local/bin/mhsendmail https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64
+      mhsendmail_bin="https://github.com/evertiro/mhsendmail/releases/download/v0.2.0-M1/mhsendmail_linux_arm64"
     fi
+    curl --silent -L -o /usr/local/bin/mhsendmail ${mhsendmail_bin}
     chmod +x /usr/local/bin/mhsendmail
     vvv_success " * MHSendmail installed"
   fi
