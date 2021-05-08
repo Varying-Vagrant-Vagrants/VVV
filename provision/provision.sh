178b386cfb33d46c9489f07f81fd0b62f0a28524
tomjn <contact@tomjn.com>
2021-05-09 00:43:33 +0100

    properly load .bash_aliases in provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -6,7 +6,7 @@
 
 # source bash_aliases before anything else so that PATH is properly configured on
 # this shell session
-. "/srv/config/bash_aliases"
+. "/srv/core/env/homedir/.bash_aliases"
 
 # cleanup
 mkdir -p /vagrant
