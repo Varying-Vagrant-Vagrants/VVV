18a089e5ebd9ffd3807650fd061bafb27f435c74
tomjn <contact@tomjn.com>
2021-05-08 14:34:41 +0100

    move config files into the env sub-provisioner

--- provision/core/env/provision.sh
+++ provision/core/env/provision.sh
@@ -66,34 +66,35 @@ function cleanup_terminal_splash() {
 function profile_setup() {
   vvv_info " * Setting ownership of files in /home/vagrant to vagrant"
   chown -R vagrant:vagrant /home/vagrant/
+
   # Copy custom dotfiles and bin file for the vagrant user from local
-  vvv_info " * Copying /srv/config/bash_profile                      to /home/vagrant/.bash_profile"
+  vvv_info " * Copying /srv/provision/core/env/homedir/.bash_profile                      to /home/vagrant/.bash_profile"
   rm -f "/home/vagrant/.bash_profile"
-  noroot cp -f "/srv/config/bash_profile" "/home/vagrant/.bash_profile"
+  noroot cp -f "/srv/provision/core/env/homedir/.bash_profile" "/home/vagrant/.bash_profile"
 
-  vvv_info " * Copying /srv/config/bash_aliases                      to /home/vagrant/.bash_aliases"
+  vvv_info " * Copying /srv/provision/core/env/homedir/.bash_aliases                      to /home/vagrant/.bash_aliases"
   rm -f "/home/vagrant/.bash_aliases"
-  noroot cp -f "/srv/config/bash_aliases" "/home/vagrant/.bash_aliases"
+  noroot cp -f "/srv/provision/core/env/homedir/.bash_aliases" "/home/vagrant/.bash_aliases"
 
-  vvv_info " * Copying /srv/config/bash_aliases                      to ${HOME}/.bash_aliases"
+  vvv_info " * Copying /srv/provision/core/env/homedir/.bash_aliases                      to ${HOME}/.bash_aliases"
   rm -f "${HOME}/.bash_aliases"
-  cp -f "/srv/config/bash_aliases" "${HOME}/.bash_aliases"
+  cp -f "/srv/provision/core/env/homedir/.bash_aliases" "${HOME}/.bash_aliases"
 
-  vvv_info " * Copying /srv/config/vimrc                             to /home/vagrant/.vimrc"
+  vvv_info " * Copying /srv/provision/core/env/homedir/.vimrc                             to /home/vagrant/.vimrc"
   rm -f "/home/vagrant/.vimrc"
-  noroot cp -f "/srv/config/vimrc" "/home/vagrant/.vimrc"
+  noroot cp -f "/srv/provision/core/env/homedir/.vimrc" "/home/vagrant/.vimrc"
 
   if [[ ! -d "/home/vagrant/.subversion" ]]; then
     noroot mkdir -p "/home/vagrant/.subversion"
   fi
 
-  vvv_info " * Copying /srv/config/subversion-servers                to /home/vagrant/.subversion/servers"
+  vvv_info " * Copying /srv/provision/core/env/homedir/.subversion/subversion-servers                to /home/vagrant/.subversion/servers"
   rm -f /home/vagrant/.subversion/servers
-  noroot cp "/srv/config/subversion-servers" "/home/vagrant/.subversion/servers"
+  noroot cp "/srv/provision/core/env/homedir/.subversion/subversion-servers" "/home/vagrant/.subversion/servers"
 
-  vvv_info " * Copying /srv/config/subversion-config                 to /home/vagrant/.subversion/config"
+  vvv_info " * Copying /srv/provision/core/env/homedir/.subversion/subversion-config                 to /home/vagrant/.subversion/config"
   rm -f /home/vagrant/.subversion/config
-  noroot cp "/srv/config/subversion-config" "/home/vagrant/.subversion/config"
+  noroot cp "/srv/provision/core/env/homedir/.subversion/subversion-config" "/home/vagrant/.subversion/config"
 
   # If a bash_prompt file exists in the VVV config/ directory, copy to the VM.
   if [[ -f "/srv/config/bash_prompt" ]]; then
@@ -101,13 +102,14 @@ function profile_setup() {
     rm -f /home/vagrant/.bash_prompt
     noroot cp "/srv/config/bash_prompt" "/home/vagrant/.bash_prompt"
   fi
+
   if [ -d "/etc/ssh" ]; then
-    vvv_info " * Copying /srv/config/ssh_known_hosts                   to /etc/ssh/ssh_known_hosts"
-    cp -f /srv/config/ssh_known_hosts /etc/ssh/ssh_known_hosts
-    vvv_info " * Copying /srv/config/sshd_config                       to /etc/ssh/sshd_config"
-    cp -f /srv/config/sshd_config /etc/ssh/sshd_config
-    vvv_info " * Reloading SSH Daemon"
+    vvv_info " * Copying /srv/provision/core/env/ssh/ssh_known_hosts                   to /etc/ssh/ssh_known_hosts"
+    cp -f /srv/provision/core/env/ssh/ssh_known_hosts /etc/ssh/ssh_known_hosts
+    vvv_info " * Copying /srv/provision/core/env/ssh/sshd_config                       to /etc/ssh/sshd_config"
+    cp -f /srv/provision/core/env/ssh/sshd_config /etc/ssh/sshd_config
     if [ "${VVV_DOCKER}" != 1 ]; then
+      vvv_info " * Reloading SSH Daemon"
       systemctl reload ssh
     fi
   fi
