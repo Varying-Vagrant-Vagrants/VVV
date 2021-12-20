ce2716e5139b63ccbea260cb140cdbbbb542113e
Tom J Nowell <contact@tomjn.com>
2021-12-20 23:37:15 +0000

    fix file check

--- provision/provision-site.sh
+++ provision/provision-site.sh
@@ -107,7 +107,7 @@ function vvv_provision_site_nginx_config() {
   fi
   sed -i "s#{upstream}#${NGINX_UPSTREAM}#" "/etc/nginx/custom-sites/${DEST_NGINX_FILE}"
 
-  if -f "/srv/certificates/${SITE_NAME}/dev.crt"; then
+  if [ -f "/srv/certificates/${SITE_NAME}/dev.crt" ]; then
     sed -i "s#{vvv_tls_cert}#ssl_certificate \"/srv/certificates/${SITE_NAME}/dev.crt\";#" "/etc/nginx/custom-sites/${DEST_NGINX_FILE}"
     sed -i "s#{vvv_tls_key}#ssl_certificate_key \"/srv/certificates/${SITE_NAME}/dev.key\";#" "/etc/nginx/custom-sites/${DEST_NGINX_FILE}"
   else
