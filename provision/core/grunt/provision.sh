f92f93c4d374c43a3ffbc7b92b0af20c6502769d
Tom J Nowell <contact@tomjn.com>
2022-01-28 20:19:26 +0000

    allow the grunt provisioner to fail on failure and tweak some commands

--- provision/core/grunt/provision.sh
+++ provision/core/grunt/provision.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 # @description Install Grunt CLI and Node/Grunt Sass
 
+set -eo pipefail
+
 function install_grunt() {
   vvv_info " * [Grunt]: Installing Grunt CLI"
   npm_config_loglevel=error npm install -g grunt grunt-cli --no-optional --force
 
   npm_config_loglevel=error hack_avoid_gyp_errors &
   vvv_info " * [Grunt]: Installing grunt-sass"
-  npm install -g grunt-sass --no-optional  --unsafe
+  npm install -g grunt-sass --no-optional --unsafe
   vvv_info " * [Grunt]: grunt-sass installed"
   touch /tmp/stop_gyp_hack
 
-  vvv_info " * [Grunt]: Installing grun-cssjanus and grunt-rtlcss"
-  npm_config_loglevel=error npm install -g grunt-cssjanus --no-optional
-  npm_config_loglevel=error npm install -g grunt-rtlcss --no-optional
+  vvv_info " * [Grunt]: Installing grunt-cssjanus and grunt-rtlcss"
+  npm_config_loglevel=error npm install -g grunt-cssjanus grunt-rtlcss --no-optional
   vvv_success " * [Grunt]: Completed Grunt installation"
 }
 
@@ -21,15 +22,14 @@ function update_grunt() {
   vvv_info " * [Grunt]: Updating Grunt CLI"
   npm_config_loglevel=error npm update -g grunt grunt-cli --no-optional --force
 
-  npm_config_loglevel=error hack_avoid_gyp_errors &
   vvv_info " * [Grunt]: Updating grunt-sass"
-  npm update -g grunt-sass  --unsafe
-  vvv_info " * [Grunt]: grunt-sass Updated"
+  hack_avoid_gyp_errors &
+  npm update -g grunt-sass --no-optional --unsafe
   touch /tmp/stop_gyp_hack
+  vvv_info " * [Grunt]: grunt-sass Updated"
 
-  vvv_info " * [Grunt]: Updating grun-cssjanus and grunt-rtlcss"
-  npm_config_loglevel=error npm update -g grunt-cssjanus --no-optional
-  npm_config_loglevel=error npm update -g grunt-rtlcss --no-optional
+  vvv_info " * [Grunt]: Updating grunt-cssjanus and grunt-rtlcss"
+  npm_config_loglevel=error npm update -g grunt-cssjanus grunt-rtlcss --no-optional
   vvv_success " * [Grunt]: Completed Grunt CLI update"
 }
 
