235852a7be2dba061e0185360f09e74817160854
Tom J Nowell <contact@tomjn.com>
2022-01-28 19:53:51 +0000

    improve grunt logging

--- provision/core/grunt/provision.sh
+++ provision/core/grunt/provision.sh
@@ -2,35 +2,35 @@
 # @description Install Grunt CLI and Node/Grunt Sass
 
 function install_grunt() {
-  vvv_info " * Installing Grunt CLI"
+  vvv_info " * [Grunt]: Installing Grunt CLI"
   npm_config_loglevel=error npm install -g grunt grunt-cli --no-optional --force
 
   npm_config_loglevel=error hack_avoid_gyp_errors &
-  vvv_info " * Installing grunt-sass"
+  vvv_info " * [Grunt]: Installing grunt-sass"
   npm install -g grunt-sass --no-optional  --unsafe
-  vvv_info " * grunt-sass installed"
+  vvv_info " * [Grunt]: grunt-sass installed"
   touch /tmp/stop_gyp_hack
 
-  vvv_info " * Installing grun-cssjanus and grunt-rtlcss"
+  vvv_info " * [Grunt]: Installing grun-cssjanus and grunt-rtlcss"
   npm_config_loglevel=error npm install -g grunt-cssjanus --no-optional
   npm_config_loglevel=error npm install -g grunt-rtlcss --no-optional
-  vvv_success " * Completed Grunt installation"
+  vvv_success " * [Grunt]: Completed Grunt installation"
 }
 
 function update_grunt() {
-  vvv_info " * Updating Grunt CLI"
+  vvv_info " * [Grunt]: Updating Grunt CLI"
   npm_config_loglevel=error npm update -g grunt grunt-cli --no-optional --force
 
   npm_config_loglevel=error hack_avoid_gyp_errors &
-  vvv_info " * Updating grunt-sass"
+  vvv_info " * [Grunt]: Updating grunt-sass"
   npm update -g grunt-sass  --unsafe
-  vvv_info " * grunt-sass Updated"
+  vvv_info " * [Grunt]: grunt-sass Updated"
   touch /tmp/stop_gyp_hack
 
-  vvv_info " * Updating grun-cssjanus and grunt-rtlcss"
+  vvv_info " * [Grunt]: Updating grun-cssjanus and grunt-rtlcss"
   npm_config_loglevel=error npm update -g grunt-cssjanus --no-optional
   npm_config_loglevel=error npm update -g grunt-rtlcss --no-optional
-  vvv_success " * Completed Grunt CLI update"
+  vvv_success " * [Grunt]: Completed Grunt CLI update"
 }
 
 # Grunt
@@ -52,14 +52,14 @@ function hack_avoid_gyp_errors() {
   # WARN EACCES user "root" sdoes not have permission to access the dev dir
   # "/usr/lib/node_modules/grunt-sass/node_modules/node-sass/.node-gyp/6.11.2"
   # TODO: Why do child processes of `npm` run as `nobody`?
-  vvv_info " * starting gyphack loop"
+  vvv_info " * [Grunt]: starting gyphack loop"
   while [ ! -f /tmp/stop_gyp_hack ]; do
     if [ -d /usr/lib/node_modules/grunt-sass/ ]; then
       chown -R nobody:vagrant /usr/lib/node_modules/grunt-sass/
     fi
     sleep .2
   done
-  vvv_info " * Stopped gyphack loop"
+  vvv_info " * [Grunt]: Stopped gyphack loop"
   rm -f /tmp/stop_gyp_hack
   return 0
 }
@@ -72,6 +72,7 @@ function grunt_setup() {
     install_grunt
   fi
 }
+
 export -f grunt_setup
 
 vvv_add_hook tools_setup_synchronous grunt_setup
