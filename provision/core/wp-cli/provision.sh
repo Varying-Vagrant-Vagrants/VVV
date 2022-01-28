42d1b83fcadfe941184a7cb95435ed86079149b0
Tom J Nowell <contact@tomjn.com>
2022-01-28 19:59:36 +0000

    better WP CLI logging

--- provision/core/wp-cli/provision.sh
+++ provision/core/wp-cli/provision.sh
@@ -3,37 +3,36 @@
 set -eo pipefail
 
 function wp_cli_setup() {
-  vvv_info " * Installing/updating WP-CLI"
-  # WP-CLI Install
-  local exists_wpcli
+  vvv_info " * [WP CLI]: Installing/updating WP CLI"
 
   # Remove old wp-cli symlink, if it exists.
   if [[ -L "/usr/local/bin/wp" ]]; then
-    vvv_info " * Removing old wp-cli symlink"
+    vvv_info " * [WP CLI]: Removing old WP CLI symlink"
     rm -f /usr/local/bin/wp
   fi
 
   if [[ ! -f "/usr/local/bin/wp" ]]; then
-    vvv_info " * Downloading wp-cli nightly, see <url>http://wp-cli.org</url>"
+    vvv_info " * [WP CLI]: Downloading WP CLI nightly, see <url>http://wp-cli.org</url>"
     noroot curl -sO https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar /tmp/wp-cli-nightly.phar
     chown -R vagrant:www-data /usr/local/bin
     noroot mv -f /tmp/wp-cli-nightly.phar /usr/local/bin/wp
     chmod +x /usr/local/bin/wp
 
-    vvv_success " * WP CLI Nightly Installed"
+    vvv_success " * [WP CLI]: WP CLI Nightly Installed"
 
-    vvv_info " * Grabbing WP CLI bash completions"
+    vvv_info " * [WP CLI]: Grabbing bash completions"
     # Install bash completions
     mkdir -p /srv/config/wp-cli/
-    vvv_info " * Downloading WP CLI bash completions"
+    vvv_info " * [WP CLI]: Downloading bash completions"
     curl -s https://raw.githubusercontent.com/wp-cli/wp-cli/master/utils/wp-completion.bash -o /srv/config/wp-cli/wp-completion.bash
     chown vagrant /srv/config/wp-cli/wp-completion.bash
+    vvv_success " * [WP CLI]: Bash completions downloaded"
   else
+    vvv_info " * [WP CLI]: Updating WP CLI Nightly"
     chown -R vagrant:www-data /usr/local/bin
     chmod +x /usr/local/bin/wp
-    vvv_info " * Updating wp-cli..."
     noroot wp cli update --nightly --yes
-    vvv_success " * WP CLI Nightly updated"
+    vvv_success " * [WP CLI]: WP CLI Nightly updated"
   fi
 
   # ensure WP CLI is owned by the right user and executable
@@ -41,13 +40,15 @@ function wp_cli_setup() {
   chmod +x /usr/local/bin/wp
 
   if [ "${VVV_DOCKER}" != 1 ]; then
-    vvv_info " * Disabling debug mods if present before running wp package installs"
+    vvv_info " * [WP-CLI]: Disabling debug mods if present before running wp package installs"
     xdebug_off
 
-    vvv_info " * Updating WP packages"
+    vvv_info " * [WP-CLI]: Updating packages"
     noroot wp package update
-    vvv_info " * WP package updates completed"
+    vvv_info " * [WP-CLI]: Package updates completed"
   fi
+
+  vvv_success " * [WP-CLI]: WP CLI setup completed"
 }
 export -f wp_cli_setup
 
