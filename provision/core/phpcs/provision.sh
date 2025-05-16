d2bdaa24eab150c6f8a98dd40a49f46adccea2bf
Tom J Nowell <contact@tomjn.com>
2025-05-16 14:20:11 +0100

    adjust phpcs provisioner to output more information on failing to set the default standard

--- provision/core/phpcs/provision.sh
+++ provision/core/phpcs/provision.sh
@@ -27,12 +27,18 @@ function php_codesniff_setup() {
   COMPOSER_RUNTIME_ENV="vagrant" composer update --no-ansi --no-progress --no-dev --prefer-dist
 
   chown -R vagrant:vagrant /srv/www/phpcs
+  chmod +x /srv/www/phpcs/bin/*
 
   vvv_info " * [PHPCS]: Setting WordPress-Core as the default PHPCodesniffer standard"
 
   # Install the standards in PHPCS
   chmod +x /srv/www/phpcs/bin/phpcs
-  noroot /srv/www/phpcs/bin/phpcs --config-set default_standard WordPress-Core
+  if noroot /srv/www/phpcs/bin/phpcs --config-set default_standard WordPress-Core; then
+    vvv_success " * [PHPCS]: Succesfully set the default standard to WordPress-Core."
+  else
+    vvv_error " ! [PHPCS]: Failed to set the default standard to WordPress-Core."
+    vvv_error " ! [PHPCS]: Permissions and owners of /src/www/phpcs/bin are as follows:\n ${ls -al /srv/www/phpcs/bin}"
+  fi
   local standards
   standards=$(noroot /srv/www/phpcs/bin/phpcs -i)
   vvv_success " * [PHPCS]: Completed with the following PHPCS standards set up: ${standards}"
